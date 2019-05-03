d05e4ffb67819f11e1efd8781106bca6d9ea38ba
dobernike <Dobernike@mail.ru>
2019-05-03 20:32:17 +0300

    Работа с setState

--- practice/react/todo/src/components/todo-list-item/todo-list-item.js
+++ practice/react/todo/src/components/todo-list-item/todo-list-item.js
@@ -6,7 +6,8 @@ import './todo-list-item.css';
 export default class TodoListItem extends Component {
 
   state = {
-    done: false
+    done: false,
+    important: false
   };
 
   onLabelClick = () => {
@@ -15,31 +16,35 @@ export default class TodoListItem extends Component {
     });
   };
 
+  onMarkImportant = () => {
+    this.setState({
+      important: true
+    })
+  }
+
   render() {
-    const { label, important = false } = this.props;
-    const { done } = this.state;
+    const { label } = this.props;
+    const { done, important } = this.state;
 
     let classNames = `todo-list-item`;
     if (done) {
       classNames += ` done`;
     }
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    if (important) {
+      classNames += ` important`;
+    }
 
     return (
       <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}
           onClick={this.onLabelClick}>
           {label}
         </span>
 
         <button type="button"
-          className="btn btn-outline-success btn-sm float-right">
+          className="btn btn-outline-success btn-sm float-right" onClick={this.onMarkImportant}>
           <i className="fa fa-exclamation" />
         </button>
 
