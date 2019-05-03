71a37824f38f986737bcba363d694ff6bae5ff91
dobernike <Dobernike@mail.ru>
2019-05-03 20:38:37 +0300

    update состояния, которое зависит от пред значения

--- practice/react/todo/src/components/todo-list-item/todo-list-item.js
+++ practice/react/todo/src/components/todo-list-item/todo-list-item.js
@@ -11,16 +11,20 @@ export default class TodoListItem extends Component {
   };
 
   onLabelClick = () => {
-    this.setState({
-      done: true
+    this.setState(({ done }) => {
+      return {
+        done: !done
+      }
     });
   };
 
   onMarkImportant = () => {
-    this.setState({
-      important: true
-    })
-  }
+    this.setState(({ important }) => {
+      return {
+        important: !important
+      }
+    });
+  };
 
   render() {
     const { label } = this.props;
