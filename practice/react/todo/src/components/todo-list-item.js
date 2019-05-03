a1bee9b6c1e5d2f12e0a4494646846e1862e882e
dobernike <Dobernike@mail.ru>
2019-05-03 14:35:09 +0300

    props - Свойства объекта

--- practice/react/todo/src/components/todo-list-item.js
+++ practice/react/todo/src/components/todo-list-item.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const TodoListItem = () => {
-    return <span>Drink Coffee</span>;
+const TodoListItem = ({ label, important = false }) => {
+
+    const style = {
+        color: important ? `tomato` : `black`
+    }
+
+    return <span style={style}>{label}</span>;
 }
 
 export default TodoListItem;
--- practice/react/todo/src/components/todo-list.js
+++ practice/react/todo/src/components/todo-list.js
@@ -3,11 +3,13 @@ import React from 'react';
 import TodoListItem from './todo-list-item';
 
 const TodoList = () => {
-    
+
     return (
         <ul>
-            <li>{<TodoListItem />}</li>
-            <li>{<TodoListItem />}</li>
+            <li>{<TodoListItem label="Drink Coffee" />}</li>
+            <li>{<TodoListItem 
+                label="Build React App" 
+                important />}</li>
         </ul>
     );
 }
