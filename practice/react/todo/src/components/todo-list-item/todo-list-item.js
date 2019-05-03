7f46c4e69abd0f612c4aef63fff756704f4a38ec
dobernike <Dobernike@mail.ru>
2019-05-03 20:26:53 +0300

    state - состояние React компонента

--- practice/react/todo/src/components/todo-list-item/todo-list-item.js
+++ practice/react/todo/src/components/todo-list-item/todo-list-item.js
@@ -5,12 +5,24 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+  state = {
+    done: false
+  };
+
   onLabelClick = () => {
-    console.log(`Done: ${this.props.label}`)
+    this.setState({
+      done: true
+    });
   };
 
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    let classNames = `todo-list-item`;
+    if (done) {
+      classNames += ` done`;
+    }
 
     const style = {
       color: important ? 'steelblue' : 'black',
@@ -18,7 +30,7 @@ export default class TodoListItem extends Component {
     };
 
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
           style={style}
