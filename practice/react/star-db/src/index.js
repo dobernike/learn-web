f0c9e3918dc1025b23f0c206f504349ae7c52b8a
dobernike <Dobernike@mail.ru>
2019-05-04 21:43:40 +0300

    Обработка ошибок fetch

--- practice/react/star-db/src/index.js
+++ practice/react/star-db/src/index.js
@@ -1,11 +1,19 @@
 
 const getResource = async (url) => {
     const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url} , received ${res.status}`);
+    }
+
     const body = await res.json();
     return body;
 };
 
-getResource(`https://swapi.co/api/people/1/`)
+getResource(`https://swapi.co/api/people/sds1/`)
     .then((body) => {
         console.log(body);
+    })
+    .catch((err) => {
+        console.log(err);
     });
