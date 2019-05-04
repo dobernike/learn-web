e34a1d79b8c7f2c3e11696bbd4f18bb29918752f
dobernike <Dobernike@mail.ru>
2019-05-04 21:36:15 +0300

    Работа с fetch

--- practice/react/star-db/src/index.js
+++ practice/react/star-db/src/index.js
@@ -1,1 +1,11 @@
-console.log(`szF`)
+
+const getResource = async (url) => {
+    const res = await fetch(url);
+    const body = await res.json();
+    return body;
+};
+
+getResource(`https://swapi.co/api/people/1/`)
+    .then((body) => {
+        console.log(body);
+    });
