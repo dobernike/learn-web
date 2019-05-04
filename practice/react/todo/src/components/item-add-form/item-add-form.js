d25617ed0c6114d23460213606ea367f97b9ba14
dobernike <Dobernike@mail.ru>
2019-05-04 16:46:19 +0300

    Контролируемые (controlled) компоненты

--- practice/react/todo/src/components/item-add-form/item-add-form.js
+++ practice/react/todo/src/components/item-add-form/item-add-form.js
@@ -17,6 +17,9 @@ export default class ItemAddForm extends Component {
     onSubmit = (e) => {
         e.preventDefault();
         this.props.onItemAdded(this.state.label)
+        this.setState({
+            label: ``
+        });
     };
 
     render() {
@@ -27,7 +30,8 @@ export default class ItemAddForm extends Component {
                 <input type="text"
                     className="form-control"
                     onChange={this.onLabelChange}
-                    placeholder="What needs to be done" />
+                    placeholder="What needs to be done"
+                    value={this.state.label} />
                 <button className="btn btn-outline-secondary">
                     Add
                 </button>
