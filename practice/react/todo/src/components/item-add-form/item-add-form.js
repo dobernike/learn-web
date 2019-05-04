9a5f8b27bd7eacef2517d0a3eedc1d25f2582936
dobernike <Dobernike@mail.ru>
2019-05-04 16:35:24 +0300

    Работа с формами

--- practice/react/todo/src/components/item-add-form/item-add-form.js
+++ practice/react/todo/src/components/item-add-form/item-add-form.js
@@ -4,15 +4,34 @@ import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
 
+    state = {
+        label: ``
+    };
 
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label)
+    };
 
     render() {
         return (
-            <div className="item-add-form">
-                <button type="button" className="btn btn-outline-secondary" onClick={() => this.props.onItemAdded(`Hello`)}>
-                    Add Item
+            <form className="item-add-form d-flex"
+                onSubmit={this.onSubmit}>
+
+                <input type="text"
+                    className="form-control"
+                    onChange={this.onLabelChange}
+                    placeholder="What needs to be done" />
+                <button className="btn btn-outline-secondary">
+                    Add
                 </button>
-            </div>
+            </form>
         )
     }
 }
