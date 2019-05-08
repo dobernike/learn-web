3178ad461e989dfdc68a1608da0ab6b579b02fb2
dobernike <dobernike@mail.ru>
2019-05-08 09:11:00 +0300

    ComponentDidUpdate + начало рефакторинга

--- practice/react/star-db/src/components/person-details/person-details.js
+++ practice/react/star-db/src/components/person-details/person-details.js
@@ -11,34 +11,74 @@ export default class PersonDetails extends Component {
     person: null
   };
 
-  updatePerson() {
+  componentDidMount() {
+    this.updatePerson();
+  }
 
+  componentDidUpdate(prevProps) {
+    if (this.props.personID !== prevProps.personId) {
+      this.updatePerson();
+    }
+  }
+
+  updatePerson() {
+    const { personId } = this.props;
+    if (!personId) {
+      return;
+    }
+
+    this.swapiService.getPerson(personId).then((person) => {
+      this.setState({ person });
+    })
   }
 
   render() {
+    if (!this.state.person) {
+      return <span>Select a person from a list</span>;
+    }
+
+    const { person } = this.state;
+    console.log(person)
+
+    const content = <PersonView person={person}/>
+
+
+
+
     return (
       <div className="person-details card">
-        <img className="person-image"
-          src="https://starwars-visualguide.com/assets/img/characters/3.jpg" alt="r2-d2" />
-
-        <div className="card-body">
-          <h4>R2-D2</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Gender</span>
-              <span>male</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Birth Year</span>
-              <span>43</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Eye Color</span>
-              <span>red</span>
-            </li>
-          </ul>
-        </div>
+        {content}
       </div>
     )
   }
+}
+
+const PersonView = (person) => {
+
+  const { id, name, gender, birthYear, eyeColor } = person;
+
+  return (
+    <React.Fragment>
+      <img className="person-image"
+        src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} alt={name} />
+
+      <div className="card-body">
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Gender</span>
+            <span>{gender}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Birth Year</span>
+            <span>{birthYear}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Eye Color</span>
+            <span>{eyeColor}</span>
+          </li>
+        </ul>
+      </div>
+    </ React.Fragment>
+  );
 }
