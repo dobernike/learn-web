ce991aa960c9a889719bd4648fcf2191999e7140
dobernike <Dobernike@mail.ru>
2019-05-12 00:47:30 +0300

    withRouter()

--- practice/react/star-db/src/components/pages/starships-page.js
+++ practice/react/star-db/src/components/pages/starships-page.js
@@ -1,26 +1,13 @@
-import React, { Component } from 'react';
-import { StarshipList, StarshipDetails } from '../sw-components';
-import Row from '../row';
+import React from 'react';
+import { StarshipList } from '../sw-components';
+import { withRouter } from 'react-router-dom';
 
-export default class PlanetPage extends Component {
-
-  state = {
-    selectedItem: null
-  };
-
-  onItemSelected = (selectedItem) => {
-    this.setState({ selectedItem });
-  };
-
-  render() {
-    const { selectedItem } = this.state;
-
-    return (
-      <Row
-        left={<StarshipList onItemSelected={this.onItemSelected} />}
-        right={<StarshipDetails itemId={selectedItem} />}
-      />
-    );
-  }
+const StarshipsPage = ({ history }) => {
+  return (
+    <StarshipList onItemSelected={(itemId) => {
+      history.push(`/starships/${itemId}`);
+    }} />
+  );
 };
 
+export default withRouter(StarshipsPage);
