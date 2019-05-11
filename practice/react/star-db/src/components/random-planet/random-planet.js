13d72f2b0e2ea2a1da586b9ebf7577e5c139d317
dobernike <Dobernike@mail.ru>
2019-05-11 23:44:22 +0300

    propTypes

--- practice/react/star-db/src/components/random-planet/random-planet.js
+++ practice/react/star-db/src/components/random-planet/random-planet.js
@@ -12,6 +12,18 @@ export default class RandomPlanet extends Component {
     updateInterval: 10000
   };
 
+  static propTypes = {
+    updateInterval: (props, propName, componentName) => {
+      const value = props[propName];
+
+      if (typeof value === `number` && !isNaN(value)){
+        return null;
+      };
+
+      return new TypeError(`${componentName}: ${propName} must be number`);
+    }
+  };
+
   swapiService = new SwapiService();
 
   state = {
