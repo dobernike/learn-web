30f124ac11a71d6783cd142e9958d1b37a1211ec
dobernike <Dobernike@mail.ru>
2019-05-12 00:11:28 +0300

    Основы react Router

--- practice/react/star-db/src/components/app/app.js
+++ practice/react/star-db/src/components/app/app.js
@@ -11,6 +11,8 @@ import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
 
 import './app.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 export default class App extends Component {
 
   state = {
@@ -33,15 +35,18 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService} >
-          <div className="stardb-app">
-            <Header onServiceChange={this.onServiceChange} />
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={this.onServiceChange} />
+              <RandomPlanet />
 
-            <RandomPlanet />
-            <PeoplePage />
-            <PlanetsPage />
-            <StarshipsPage />
 
-          </div>
+              <Route path="/people" component={PeoplePage} />
+              <Route path="/planets" component={PlanetsPage} />
+              <Route path="/starships" component={StarshipsPage} />
+
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
