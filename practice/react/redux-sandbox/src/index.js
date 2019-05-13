b279e5d41764c322497da0315e7ab4b18f8730d0
dobernike <dobernike@mail.ru>
2019-05-14 00:08:41 +0300

    Структура проекта

--- practice/react/redux-sandbox/src/index.js
+++ practice/react/redux-sandbox/src/index.js
@@ -1,30 +1,8 @@
 import { createStore } from 'redux';
-
-const reducer = (state = 0, action) => {
-
-    switch (action.type) {
-        case `INC`:
-            return state + 1;
-
-        case `DEC`:
-            return state - 1;
-
-        case `RND`:
-            return state + action.payload;
-
-        default:
-            return state;
-    }
-};
+import reducer from './reducer';
+import { inc, dec, rnd } from './actions'
 
 const store = createStore(reducer);
-
-const inc = () => ({ type: `INC` })
-
-const dec = () => ({ type: `DEC` })
-
-const rnd = (payload) => ({ type: `RND`, payload })
-
 
 document.getElementById(`inc`).addEventListener(`click`, () => {
     store.dispatch(inc())
---
+++ practice/react/redux-sandbox/src/reducer.js
@@ -0,0 +1,18 @@
+const reducer = (state = 0, action) => {
+
+    switch (action.type) {
+        case `INC`:
+            return state + 1;
+
+        case `DEC`:
+            return state - 1;
+
+        case `RND`:
+            return state + action.payload;
+
+        default:
+            return state;
+    }
+};
+
+export default reducer;
