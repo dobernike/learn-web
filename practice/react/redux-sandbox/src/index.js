2d3af227314449965e958cd1ae370edb5ad1123d
dobernike <dobernike@mail.ru>
2019-05-13 23:58:29 +0300

    UI для Redux

--- practice/react/redux-sandbox/src/index.js
+++ practice/react/redux-sandbox/src/index.js
@@ -6,16 +6,26 @@ const reducer = (state = 0, action) => {
         case `INC`:
             return state + 1;
 
+        case `DEC`:
+            return state - 1;
+
         default:
             return state;
     }
 };
 
 const store = createStore(reducer);
-store.subscribe(() => {
-    console.log(store.getState());
+
+document.getElementById(`inc`).addEventListener(`click`, () => {
+    store.dispatch({ type: `INC` })
 });
 
-store.dispatch({type: `INC`});
-store.dispatch({type: `INC`});
-store.dispatch({type: `INC`});
+document.getElementById(`dec`).addEventListener(`click`, () => {
+    store.dispatch({ type: `DEC` })
+});
+
+const update = () => {
+    document.getElementById(`counter`).innerHTML = store.getState();
+};
+
+store.subscribe(update);
