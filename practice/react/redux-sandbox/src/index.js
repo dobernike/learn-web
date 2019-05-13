9465dcef01c958c29b4c2745a9dd574bdf8838b2
dobernike <dobernike@mail.ru>
2019-05-14 00:06:23 +0300

    Action Creator

--- practice/react/redux-sandbox/src/index.js
+++ practice/react/redux-sandbox/src/index.js
@@ -19,20 +19,24 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
+const inc = () => ({ type: `INC` })
+
+const dec = () => ({ type: `DEC` })
+
+const rnd = (payload) => ({ type: `RND`, payload })
+
+
 document.getElementById(`inc`).addEventListener(`click`, () => {
-    store.dispatch({ type: `INC` })
+    store.dispatch(inc())
 });
 
 document.getElementById(`dec`).addEventListener(`click`, () => {
-    store.dispatch({ type: `DEC` })
+    store.dispatch(dec())
 });
 
 document.getElementById(`rnd`).addEventListener(`click`, () => {
     const payload = Math.floor(Math.random() * 10);
-    store.dispatch({
-        type: `RND`,
-        payload
-    })
+    store.dispatch(rnd(payload));
 });
 
 const update = () => {
