7ade420553d834098c3e9eca6f2263af0a220a1a
dobernike <dobernike@mail.ru>
2019-05-13 22:43:28 +0300

    reducer

--- practice/react/redux-sandbox/src/index.js
+++ practice/react/redux-sandbox/src/index.js
@@ -1,1 +1,16 @@
-console.log(`fgasdg`)
+
+const reducer = (state = 0, action) => {
+
+    switch (action.type) {
+        case `INC`:
+            return state + 1;
+
+        default:
+            return state;
+    }
+};
+
+let state = reducer(undefined, {});
+
+state = reducer(state, { type: `INC` });
+console.log(state);
