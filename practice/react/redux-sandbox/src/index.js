f837a33973a6194da281ee333b7b0e51cbf69fa9
dobernike <Dobernike@mail.ru>
2019-05-14 13:47:52 +0300

    bindActionCreators

--- practice/react/redux-sandbox/src/index.js
+++ practice/react/redux-sandbox/src/index.js
@@ -1,20 +1,19 @@
-import { createStore } from 'redux';
+import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
-import { inc, dec, rnd } from './actions'
+import * as actions from './actions'
 
 const store = createStore(reducer);
+const { dispatch } = store;
 
-document.getElementById(`inc`).addEventListener(`click`, () => {
-    store.dispatch(inc())
-});
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
-document.getElementById(`dec`).addEventListener(`click`, () => {
-    store.dispatch(dec())
-});
+document.getElementById(`inc`).addEventListener(`click`, inc);
+
+document.getElementById(`dec`).addEventListener(`click`, dec);
 
 document.getElementById(`rnd`).addEventListener(`click`, () => {
     const payload = Math.floor(Math.random() * 10);
-    store.dispatch(rnd(payload));
+    rnd(payload);
 });
 
 const update = () => {
