c3fb353a1b05c4e3dd46dd973b0082c01c67ea0f
dobernike <Dobernike@mail.ru>
2019-05-15 12:18:12 +0300

    React + Redux

--- practice/react/redux-sandbox/src/index.js
+++ practice/react/redux-sandbox/src/index.js
@@ -13,14 +13,17 @@ const { dispatch } = store;
 const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
 const update = () => {
+    ReactDOM.render(<Counter
+        counter={store.getState()}
+        inc={inc}
+        dec={dec}
+        rnd={() => {
+            const value = Math.floor(Math.random() * 10);
+            rnd(value);
+        }}
+    />,
+        document.getElementById(`root`));
 };
 
+update();
 store.subscribe(update);
-
-ReactDOM.render(<Counter
-    counter={store.getState()}
-    inc={inc}
-    dec={dec}
-    rnd={rnd}
-/>,
-    document.getElementById(`root`));
