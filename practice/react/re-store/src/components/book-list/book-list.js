d66d2cf3dc264e26202d16b2d12d04a10945d16b
dobernike <dobernike@mail.ru>
2019-05-21 23:42:36 +0300

    Компоненты-контейнеры

--- practice/react/re-store/src/components/book-list/book-list.js
+++ practice/react/re-store/src/components/book-list/book-list.js
@@ -8,7 +8,21 @@ import { compose } from '../../utils';
 import Spinner from '../spinner';
 import ErrorIndicator from '../error-indicator';
 
-class BookList extends Component {
+const BookList = ({ books }) => {
+  return (
+    <ul className="book-list">
+      {
+        books.map((book) => {
+          return (
+            <li key={book.id}><BookListItem book={book} /></li>
+          )
+        })
+      }
+    </ul>
+  );
+};
+
+class BookListContainer extends Component {
 
   componentDidMount() {
     this.props.fetchBooks();
@@ -18,24 +32,14 @@ class BookList extends Component {
     const { books, loading, error } = this.props;
 
     if (loading) {
-      return <Spinner />
+      return <Spinner />;
     }
 
     if (error) {
-      return <ErrorIndicator />
+      return <ErrorIndicator />;
     }
 
-    return (
-      <ul className="book-list">
-        {
-          books.map((book) => {
-            return (
-              <li key={book.id}><BookListItem book={book} /></li>
-            )
-          })
-        }
-      </ul>
-    );
+    return <BookList books={books} />;
   };
 };
 
@@ -54,4 +58,4 @@ const mapDispatchToProps = (dispatch, { bookstoreService }) => {
 export default compose(
   withBookstoreService(),
   connect(mapStateToProps, mapDispatchToProps)
-)(BookList);
+)(BookListContainer);
