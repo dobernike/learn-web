c366f79167bbb05cab822e527fb58ebf770b849a
dobernike <Dobernike@mail.ru>
2019-05-24 13:55:44 +0300

    Middleware

--- practice/react/re-store/src/store.js
+++ practice/react/re-store/src/store.js
@@ -1,7 +1,24 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const logMiddleware = ({ getState}) => (next) => (action) => {
+  console.log(action.type, getState());
+  return next(action);
+};
+
+const stringMiddleware = () => (next) => (action) => {
+  if (typeof action === `string`) {
+    return next({
+      type: action
+    });
+  }
+
+  return next(action);
+};
+
+const store = createStore(reducer, applyMiddleware(
+  stringMiddleware, logMiddleware));
+
 
 export default store;
