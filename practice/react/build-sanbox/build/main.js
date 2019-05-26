2162f22237fe8a249ae19ecf0f0fa23c0f5c11f9
dobernike <dobernike@mail.ru>
2019-05-26 09:45:31 +0300

    Плагины Babel

--- practice/react/build-sanbox/build/main.js
+++ practice/react/build-sanbox/build/main.js
@@ -1,10 +1,26 @@
-class App {
-  run() {
-    const name = `World`;
-    console.log(`Hello ${name}`);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var App =
+/*#__PURE__*/
+function () {
+  function App() {
+    _classCallCheck(this, App);
   }
 
-}
+  _createClass(App, [{
+    key: "run",
+    value: function run() {
+      var name = "World";
+      console.log("Hello ".concat(name));
+    }
+  }]);
 
-const app = new App();
+  return App;
+}();
+
+var app = new App();
 app.run();
