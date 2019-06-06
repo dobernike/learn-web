e450123736975cd9d8e79c38c243dcb6332dba88
dobernike <dobernike@mail.ru>
2019-06-07 01:16:43 +0300

    работа JSX

--- practice/react/theory/src/App.js
+++ practice/react/theory/src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello again</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>hello again</h1>
+      </div>
+    );
+
+  //   return React.createElement(
+  //     "div",
+  //     {
+  //       className: 'App'
+  //     },
+  //     React.createElement("h1", null, "Hello again")
+  //   );
+  }
 }
 
 export default App;
