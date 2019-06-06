cf31a8c1748fe31d04e48c9f79017db00eaf6e61
dobernike <dobernike@mail.ru>
2019-06-07 01:39:18 +0300

    inline стили

--- practice/react/theory/src/App.js
+++ practice/react/theory/src/App.js
@@ -3,19 +3,23 @@ import "./App.css";
 
 class App extends Component {
   render() {
+    const divStyle = {
+     textAlign: "center"
+    };
+
     return (
-      <div className="App">
-        <h1>hello again</h1>
+      <div style={divStyle}>
+        <h1 style={{color: 'blue', fontSize: '20px'}}>hello again</h1>
       </div>
     );
 
-  //   return React.createElement(
-  //     "div",
-  //     {
-  //       className: 'App'
-  //     },
-  //     React.createElement("h1", null, "Hello again")
-  //   );
+    //   return React.createElement(
+    //     "div",
+    //     {
+    //       className: 'App'
+    //     },
+    //     React.createElement("h1", null, "Hello again")
+    //   );
   }
 }
 
