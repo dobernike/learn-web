462369a686fe6f4fe866dab4fa7d5b2ee4fb2823
dobernike <dobernike@mail.ru>
2019-06-09 00:05:46 +0300

    Вывод динамических данных

--- practice/react/theory/src/Car/Car.js
+++ practice/react/theory/src/Car/Car.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-// function Car() {
-//   return <h2>This is Car</h2>;
-// }
-
-// const Car = () => {
-//   return <h2>This is Car</h2>;
-// }
-
-// const Car = () => <h2>This is Car</h2>;
-
-export default () => <h2>This is Car</h2>;
+export default () => (
+  <h2>
+    <p>This is Car</p>
+    <p>
+      Number:
+      <strong>{Math.floor(Math.random() * 100)}</strong>
+    </p>
+  </h2>
+);
