d2e1588522883869a3007476b2b3a50e24b4e70e
dobernike <dobernike@mail.ru>
2019-06-08 23:47:46 +0300

    Создание простого компонента

--- practice/react/theory/src/App.js
+++ practice/react/theory/src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
+import Car from "./Car/Car";
 
 class App extends Component {
   render() {
     const divStyle = {
-     textAlign: "center"
+      textAlign: "center"
     };
 
     return (
       <div style={divStyle}>
-        <h1 style={{color: 'blue', fontSize: '20px'}}>hello again</h1>
+        <h1>hello again</h1>
+        <Car />
       </div>
     );
-
-    //   return React.createElement(
-    //     "div",
-    //     {
-    //       className: 'App'
-    //     },
-    //     React.createElement("h1", null, "Hello again")
-    //   );
   }
 }
 
---
+++ practice/react/theory/src/Car/Car.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// function Car() {
+//   return <h2>This is Car</h2>;
+// }
+
+// const Car = () => {
+//   return <h2>This is Car</h2>;
+// }
+
+// const Car = () => <h2>This is Car</h2>;
+
+export default () => <h2>This is Car</h2>;
