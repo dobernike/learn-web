7deb5c2bfabcf62479d05e98913903ba1c2ea4ae
dobernike <dobernike@mail.ru>
2019-06-11 23:43:25 +0300

    Создание state

--- practice/react/theory/src/App.js
+++ practice/react/theory/src/App.js
@@ -3,22 +3,29 @@ import "./App.css";
 import Car from "./Car/Car";
 
 class App extends Component {
+  state = {
+    cars: [
+      { name: "Ford", year: "2013" },
+      { name: "Vw", year: "1993" },
+      { name: "Audi", year: "2018" }
+    ],
+    pageTitle: "React Components"
+  };
+
   render() {
     const divStyle = {
       textAlign: "center"
     };
 
+    const cars = this.state.cars;
+
     return (
       <div style={divStyle}>
-        <h1>hello again</h1>
+        <h1>{this.state.pageTitle}</h1>
 
-        <Car name={"Ford"} year={2013}>
-          <p style={{color: 'blue'}}>COLOR</p>
-        </Car>
-        <Car name="Vw" year="1993">
-        <p style={{color: 'red'}}>COLOR</p>
-        </Car>
-        <Car name={"Audi"} year={"2018"} />
+        <Car name={cars[0].name} year={cars[0].year} />
+        <Car name={cars[1].name} year={cars[1].year} />
+        <Car name={cars[2].name} year={cars[2].year} />
       </div>
     );
   }
