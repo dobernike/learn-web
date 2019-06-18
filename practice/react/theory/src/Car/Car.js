0cf06ccb359856f5d53d66471bc563643236ecbb
dobernike <Dobernike@mail.ru>
2019-06-18 23:33:32 +0300

    Создание Stateful компонента

--- practice/react/theory/src/Car/Car.js
+++ practice/react/theory/src/Car/Car.js
@@ -1,41 +1,37 @@
 import React from "react";
-// import Radium from 'radium';
 import "./Car.css";
 
-const Car = props => {
-  const inputClasses = [`input`];
+class Car extends React.Component {
+  render() {
+    const inputClasses = [`input`];
 
-  if (props.name !== ``) {
-    inputClasses.push(`green`);
-  } else {
-    inputClasses.push(`red`);
+    if (this.props.name !== ``) {
+      inputClasses.push(`green`);
+    } else {
+      inputClasses.push(`red`);
+    }
+
+    if (this.props.name.length > 3) {
+      inputClasses.push(`bold`);
+    }
+
+    return (
+      <div className="Car">
+        <h3>Car name is: {this.props.name}</h3>
+        <p>
+          Year:
+          <strong>{this.props.year}</strong>
+        </p>
+        <input
+          className={inputClasses.join(` `)}
+          type="text"
+          onChange={this.props.onChangeName}
+          value={this.props.name}
+        />
+        <button onClick={this.props.onDelete}>Delete</button>
+      </div>
+    );
   }
+}
 
-  if (props.name.length > 3) {
-    inputClasses.push(`bold`);
-  }
-
-  // const style = {
-
-  // };
-
-  return (
-    <div className="Car">
-      <h3>Car name is: {props.name}</h3>
-      <p>
-        Year:
-        <strong>{props.year}</strong>
-      </p>
-      <input
-        className={inputClasses.join(` `)}
-        type="text"
-        onChange={props.onChangeName}
-        value={props.name}
-      />
-      <button onClick={props.onDelete}>Delete</button>
-    </div>
-  );
-};
-
-// export default Radium(Car);
 export default Car;
