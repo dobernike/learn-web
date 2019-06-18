95a263a261f9ce27787a2f81b0c699a663a77ff2
dobernike <Dobernike@mail.ru>
2019-06-18 22:16:24 +0300

    Подключение css

--- practice/react/theory/src/Car/Car.js
+++ practice/react/theory/src/Car/Car.js
@@ -1,20 +1,14 @@
 import React from "react";
+import "./Car.css";
 
 export default props => (
-  <div style={{
-    border: `1px solid #ccc`,
-    marginBottom: `10px`,
-    display: `block`,
-    padding: `10px`,
-    boxShadow: `0 4px 5px 0 rgba(0, 0, 0, .14)`,
-    borderRadius: `5px`
-  }}>
+  <div className="Car">
     <h3>Car name is: {props.name}</h3>
     <p>
       Year:
       <strong>{props.year}</strong>
     </p>
-    <input type="text" onChange={props.onChangeName} value={props.name}/>
+    <input type="text" onChange={props.onChangeName} value={props.name} />
     <button onClick={props.onDelete}>Delete</button>
   </div>
 );
