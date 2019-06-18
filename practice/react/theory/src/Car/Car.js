e0524f28ee3dc3d0917b5275a58a536fda9029a4
dobernike <Dobernike@mail.ru>
2019-06-18 22:41:33 +0300

    Radium

--- practice/react/theory/src/Car/Car.js
+++ practice/react/theory/src/Car/Car.js
@@ -1,7 +1,8 @@
 import React from "react";
+// import Radium from 'radium';
 import "./Car.css";
 
-export default props => {
+const Car = props => {
   const inputClasses = [`input`];
 
   if (props.name !== ``) {
@@ -14,8 +15,18 @@ export default props => {
     inputClasses.push(`bold`);
   }
 
+  const style = {
+    border: `1px solid #ccc`,
+    boxShadow: `0 4px 5px 0 rgba(0, 0, 0, .14)`,
+    ":hover": {
+      border: `1px solid #aaa`,
+      boxShadow: `0 4px 15px 0 rgba(0, 0, 0, .25)`,
+      cursor: `pointer`
+    }
+  };
+
   return (
-    <div className="Car">
+    <div className="Car" style={style}>
       <h3>Car name is: {props.name}</h3>
       <p>
         Year:
@@ -31,3 +42,6 @@ export default props => {
     </div>
   );
 };
+
+// export default Radium(Car);
+export default Car;
