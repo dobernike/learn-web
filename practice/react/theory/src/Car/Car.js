86bdf6d94bb472ad777abb0bb1e5b410537c9281
dobernike <Dobernike@mail.ru>
2019-06-18 23:56:23 +0300

    Жизнененный цикл удаления

--- practice/react/theory/src/Car/Car.js
+++ practice/react/theory/src/Car/Car.js
@@ -19,6 +19,10 @@ class Car extends React.Component {
     console.log(`Car componentDidUpdate`);
   }
 
+  componentWillUnmount() {
+    console.log(`Car componentWillUnmount`);
+  }
+
   render() {
     console.log(`Car render`);
     const inputClasses = [`input`];
