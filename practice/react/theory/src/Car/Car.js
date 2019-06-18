5f48a30cae985fb0b1f4db05a2d8f8e3f0358505
dobernike <Dobernike@mail.ru>
2019-06-18 22:30:35 +0300

    Динамические классы

--- practice/react/theory/src/Car/Car.js
+++ practice/react/theory/src/Car/Car.js
@@ -1,14 +1,33 @@
 import React from "react";
 import "./Car.css";
 
-export default props => (
-  <div className="Car">
-    <h3>Car name is: {props.name}</h3>
-    <p>
-      Year:
-      <strong>{props.year}</strong>
-    </p>
-    <input type="text" onChange={props.onChangeName} value={props.name} />
-    <button onClick={props.onDelete}>Delete</button>
-  </div>
-);
+export default props => {
+  const inputClasses = [`input`];
+
+  if (props.name !== ``) {
+    inputClasses.push(`green`);
+  } else {
+    inputClasses.push(`red`);
+  }
+
+  if (props.name.length > 3) {
+    inputClasses.push(`bold`);
+  }
+
+  return (
+    <div className="Car">
+      <h3>Car name is: {props.name}</h3>
+      <p>
+        Year:
+        <strong>{props.year}</strong>
+      </p>
+      <input
+        className={inputClasses.join(` `)}
+        type="text"
+        onChange={props.onChangeName}
+        value={props.name}
+      />
+      <button onClick={props.onDelete}>Delete</button>
+    </div>
+  );
+};
