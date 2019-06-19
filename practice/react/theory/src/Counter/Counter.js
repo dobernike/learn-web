d4b68bd8ff8332b35e5613d1cd45b013adc5ae72
dobernike <Dobernike@mail.ru>
2019-06-19 11:09:55 +0300

    Правильное изменение State

--- practice/react/theory/src/Counter/Counter.js
+++ practice/react/theory/src/Counter/Counter.js
@@ -7,8 +7,13 @@ export default class Counter extends Component {
   };
 
   addCounter = () => {
-    this.setState({
-      counter: this.state.counter + 1
+    // this.setState({
+    //   counter: this.state.counter + 1
+    // });
+    this.setState(prevState => {
+      return {
+        counter: prevState.counter + 1
+      };
     });
   };
 
