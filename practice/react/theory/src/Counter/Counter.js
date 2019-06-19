2a5aba7edcb03e674a5f94dfb30d2dce82625acc
dobernike <Dobernike@mail.ru>
2019-06-19 11:06:12 +0300

    Фрагменты 2

--- practice/react/theory/src/Counter/Counter.js
+++ practice/react/theory/src/Counter/Counter.js
@@ -1,29 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import Auxiliary from "../hoc/Auxiliary";
 
 export default class Counter extends Component {
   state = {
     counter: 0
-  }
+  };
 
   addCounter = () => {
     this.setState({
       counter: this.state.counter + 1
-    })
-  }
+    });
+  };
 
   render() {
     // return (
-    //   <div>
+    //   <Auxiliary>
     //     <h2>Counter {this.state.counter}</h2>
     //     <button onClick={this.addCounter}>+</button>
     //     <button onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
-    //   </div>
+    //   </Auxiliary>
     // )
 
-    return [
-          <h2 key={`1`}>Counter {this.state.counter}</h2>,
-          <button key={`2`} onClick={this.addCounter}>+</button>,
-          <button key={`3`} onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
-    ]
+    // return [
+    //       <h2 key={`1`}>Counter {this.state.counter}</h2>,
+    //       <button key={`2`} onClick={this.addCounter}>+</button>,
+    //       <button key={`3`} onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
+    // ]
+
+    // return (
+    //   <React.Fragment>
+    //     <h2>Counter {this.state.counter}</h2>
+    //     <button onClick={this.addCounter}>+</button>
+    //     <button onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
+    //   </React.Fragment>
+    // )
+
+    return (
+      <>
+        <h2>Counter {this.state.counter}</h2>
+        <button onClick={this.addCounter}>+</button>
+        <button
+          onClick={() => this.setState({ counter: this.state.counter - 1 })}
+        >
+          -
+        </button>
+      </>
+    );
   }
 }
