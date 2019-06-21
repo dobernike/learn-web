adb0469a7441da495d4d734831e13844f6df5997
dobernike <Dobernike@mail.ru>
2019-06-21 21:59:40 +0300

    Проверка правильности

--- practice/react/react-quiz/src/containers/Quiz/Quiz.js
+++ practice/react/react-quiz/src/containers/Quiz/Quiz.js
@@ -37,6 +37,13 @@ class Quiz extends Component {
   }
 
   onAnswerClickHandler = answerId => {
+    if (this.state.answerState) {
+      const key = Object.keys(this.state.answerState)[0];
+      if (this.state.answerState[key] === `success`) {
+        return;
+      }
+    }
+
     const question = this.state.quiz[this.state.activeQuestion];
 
     if (question.rightAnswerId === answerId) {
