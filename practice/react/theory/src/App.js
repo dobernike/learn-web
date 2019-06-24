bf5143578caa64f9d7c8be2a9336cca0a68005d0
dobernike <Dobernike@mail.ru>
2019-06-24 21:13:30 +0300

    Навигация между страницами

--- practice/react/theory/src/App.js
+++ practice/react/theory/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.scss";
 import About from "./About/About";
 import Cars from "./Cars/Cars";
-import { Route } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -11,13 +11,13 @@ class App extends Component {
         <nav className="nav">
           <ul>
             <li>
-              <a href="/">Home</a>
+              <NavLink to="/">Home</NavLink>
             </li>
             <li>
-              <a href="/about">About</a>
+              <NavLink to="/about">About</NavLink>
             </li>
             <li>
-              <a href="/cars">Cars</a>
+              <NavLink to="/cars">Cars</NavLink>
             </li>
           </ul>
         </nav>
