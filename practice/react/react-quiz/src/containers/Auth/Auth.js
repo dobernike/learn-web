a3195b72559a494d86a170cc26665a21e104d024
dobernike <Dobernike@mail.ru>
2019-06-28 20:15:52 +0300

    Страница авторизации

--- practice/react/react-quiz/src/containers/Auth/Auth.js
+++ practice/react/react-quiz/src/containers/Auth/Auth.js
@@ -1,11 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import "./Auth.css";
+import Button from "../../components/UI/Button/Button";
 
 export default class Auth extends Component {
+  loginHandler = () => {};
+
+  registerHandler = () => {};
+
+  submitHandler = event => event.preventDefault();
+
   render() {
-    return(
-      <div>
-        <h1>Auth</h1>
+    return (
+      <div className="Auth">
+        <div>
+          <h1>Авторизация</h1>
+          <form className="AuthForm" onSubmit={this.submitHandler}>
+            <input type="text" />
+            <input type="text" />
+            <Button type="success" onClick={this.loginHandler}>
+              Войти
+            </Button>
+            <Button type="primary" onClick={this.registerHandler}>
+              Регистрация
+            </Button>
+          </form>
+        </div>
       </div>
-    )
+    );
   }
 }
