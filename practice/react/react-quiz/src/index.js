921779e6e7cc073745de2be98539f0e7b6b761ca
dobernike <Dobernike@mail.ru>
2019-06-28 17:02:54 +0300

    Настройка роутера

--- practice/react/react-quiz/src/index.js
+++ practice/react/react-quiz/src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
 
 serviceWorker.unregister();
