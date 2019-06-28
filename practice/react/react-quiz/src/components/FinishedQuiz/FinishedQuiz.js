9123eb586ada89405a7ac2aeeec14a1730a73684
dobernike <Dobernike@mail.ru>
2019-06-28 19:45:52 +0300

    Ссылка через кастомный компонент

--- practice/react/react-quiz/src/components/FinishedQuiz/FinishedQuiz.js
+++ practice/react/react-quiz/src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./FinishedQuiz.css";
+import { Link } from "react-router-dom";
 import Button from "../UI/Button/Button";
 
 const FinishedQuiz = props => {
@@ -34,7 +35,9 @@ const FinishedQuiz = props => {
         <Button onClick={props.onRetry} type="primary">
           Повторить
         </Button>
-        <Button type="success">Перейти в список тестов</Button>
+        <Link to="/">
+          <Button type="success">Перейти в список тестов</Button>
+        </Link>
       </div>
     </div>
   );
