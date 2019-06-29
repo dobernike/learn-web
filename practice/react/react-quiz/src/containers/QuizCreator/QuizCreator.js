3d3d3a22374b683936652e29a5dddf4596167334
dobernike <Dobernike@mail.ru>
2019-06-29 11:56:45 +0300

    Создание тестов

--- practice/react/react-quiz/src/containers/QuizCreator/QuizCreator.js
+++ practice/react/react-quiz/src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
+import "./QuizCreator.css";
+import Button from "../../components/UI/Button/Button";
 
 export default class QuizCreator extends Component {
+  submitHandler = event => event.preventDefault();
+
+  addQuestionHandler = () => {};
+
+  createQuizHandler = () => {};
+
   render() {
     return (
-      <div>
-        <h1>Quiz Creator</h1>
+      <div className="QuizCreator">
+        <div>
+          <h1>Quiz Creator</h1>
+
+          <form onSubmit={this.submitHandler}>
+            <input />
+            <hr />
+            <input />
+            <input />
+            <input />
+            <input />
+            <select />
+            <Button type="primary" onClick={this.addQuestionHandler}>
+              Добавить вопрос
+            </Button>
+            <Button type="success" onClick={this.createQuizHandler}>
+              Создать тест
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
