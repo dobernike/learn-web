15c34113e37364a78ffd3e6dce66d9b7e87d3a94
dobernike <Dobernike@mail.ru>
2019-06-29 11:21:37 +0300

    Валидация 2

--- practice/react/react-quiz/src/containers/Auth/Auth.js
+++ practice/react/react-quiz/src/containers/Auth/Auth.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import "./Auth.css";
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
+// import is from 'is_js';
+
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
 export default class Auth extends Component {
   state = {
@@ -39,8 +45,40 @@ export default class Auth extends Component {
 
   submitHandler = event => event.preventDefault();
 
+  validateControl(value, validation) {
+    if (!validation) {
+      return true;
+    }
+
+    let isValid = true;
+
+    if (validation.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (validation.email) {
+      // isValid = is.email(value) && isValid;
+      isValid = validateEmail(value) && isValid;
+    }
+
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid;
+    }
+
+    return isValid;
+  }
+
   onChangeHandler = (event, controlName) => {
-    console.log(event.target.value, controlName);
+    const formControls = { ...this.state.formControls };
+    const control = { ...formControls[controlName] };
+
+    control.value = event.target.value;
+    control.touched = true;
+    control.valid = this.validateControl(control.value, control.validation);
+
+    formControls[controlName] = control;
+
+    this.setState({ formControls });
   };
 
   renderInputs() {
