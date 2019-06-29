4caca5b75452ddbc1e9c17789a7ee92f70a6e4ae
dobernike <Dobernike@mail.ru>
2019-06-30 00:20:57 +0300

    Добавление вопроса

--- practice/react/react-quiz/src/containers/QuizCreator/QuizCreator.js
+++ practice/react/react-quiz/src/containers/QuizCreator/QuizCreator.js
@@ -50,9 +50,58 @@ export default class QuizCreator extends Component {
 
   addQuestionHandler = event => {
     event.preventDefault();
+
+    const quiz = this.state.quiz.concat();
+    const idx = quiz.length + 1;
+
+    const {
+      question,
+      option1,
+      option2,
+      option3,
+      option4
+    } = this.state.formControls;
+
+    const questionItem = {
+      question: question.value,
+      id: idx,
+      rightAnswerId: this.state.rightAnswerId,
+      answers: [
+        {
+          text: option1.value,
+          id: option1.id
+        },
+        {
+          text: option2.value,
+          id: option2.id
+        },
+        {
+          text: option3.value,
+          id: option3.id
+        },
+        {
+          text: option4.value,
+          id: option4.id
+        }
+      ]
+    };
+
+    quiz.push(questionItem);
+
+    this.setState({
+      quiz,
+      isFormValid: false,
+      rightAnswerId: 1,
+      formControls: createFormControls()
+    });
   };
 
-  createQuizHandler = () => {};
+  createQuizHandler = event => {
+    event.preventDefault();
+
+    console.log(this.state.quiz);
+    // TODO: Server
+  };
 
   changeHandler = (value, controlName) => {
     const formControls = { ...this.state.formControls };
