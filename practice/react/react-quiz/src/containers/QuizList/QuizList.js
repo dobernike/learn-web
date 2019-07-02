258a1f0c5442c92a381cf0ab5566f11b17bd87c7
dobernike <Dobernike@mail.ru>
2019-07-03 00:33:20 +0300

    Загрузка списка тестов

--- practice/react/react-quiz/src/containers/QuizList/QuizList.js
+++ practice/react/react-quiz/src/containers/QuizList/QuizList.js
@@ -4,19 +4,38 @@ import { NavLink } from "react-router-dom";
 import axios from "axios";
 
 export default class QuizList extends Component {
+  state = {
+    quizes: []
+  };
   renderQuizes() {
-    return [1, 2, 3].map((quiz, idx) => {
+    return this.state.quizes.map(quiz => {
       return (
-        <li key={idx}>
-          <NavLink to={"/quiz/" + quiz}>Тест {quiz}</NavLink>
+        <li key={quiz.id}>
+          <NavLink to={"/quiz/" + quiz.id}>{quiz.name}</NavLink>
         </li>
       );
     });
   }
 
-  componentDidMount() {
-    axios.get(`https://react-quizes-161ef.firebaseio.com/quiz.json`).then(response => console.log(response)
-    )
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        `https://react-quizes-161ef.firebaseio.com/quizes.json`
+      );
+
+      const quizes = [];
+
+      Object.keys(response.data).forEach((key, idx) => {
+        quizes.push({
+          id: key,
+          name: `Тест № ${idx + 1}`
+        });
+      });
+
+      this.setState({ quizes });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   render() {
