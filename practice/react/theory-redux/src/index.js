6939b1be9326d8df839615934c09fb7f791e1010
dobernike <dobernike@mail.ru>
2019-07-06 20:56:28 +0300

    React и Redux

--- practice/react/theory-redux/src/index.js
+++ practice/react/theory-redux/src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./redux/rootReducer";
 
+const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
 registerServiceWorker();
---
+++ practice/react/theory-redux/src/redux/rootReducer.js
@@ -0,0 +1,8 @@
+
+const initialState = {
+  counter: 0
+};
+
+export default function rootReducer(state = initialState, action) {
+  return state;
+}
