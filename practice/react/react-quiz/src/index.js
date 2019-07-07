9501c1f7e23a418ccf6a034f051410b692176c3e
dobernike <dobernike@mail.ru>
2019-07-07 13:37:00 +0300

    Настройка приложения под redux

--- practice/react/react-quiz/src/index.js
+++ practice/react/react-quiz/src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { createStore, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
+import rootReducer from "./store/reducers/rootReducer";
+import thunk from "redux-thunk";
+
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
