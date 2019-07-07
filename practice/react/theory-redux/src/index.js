e947ce562c2a5a2123f420794c3c9505941d55a4
dobernike <dobernike@mail.ru>
2019-07-07 12:36:16 +0300

    Работа Middleware

--- practice/react/theory-redux/src/index.js
+++ practice/react/theory-redux/src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./redux/rootReducer";
 
-const store = createStore(rootReducer);
+// function loggerMiddleware(store) {
+//   return function(next) {
+//     return function(action) {
+//       const result = next(action);
+//       console.log("Middleware", store.getState());
+
+//       return result;
+//     };
+//   };
+// }
+
+const loggerMiddleware = store => next => action => {
+  const result = next(action);
+  console.log("Middleware", store.getState());
+  return result;
+};
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 const app = (
   <Provider store={store}>
