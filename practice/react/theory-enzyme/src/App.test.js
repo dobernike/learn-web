10e3c3d8b6f79b1ccb0cdba303bc763d18a551a4
dobernike <dobernike@mail.ru>
2019-07-09 23:13:22 +0300

    Создание тестов

--- practice/react/theory-enzyme/src/App.test.js
+++ practice/react/theory-enzyme/src/App.test.js
@@ -1,10 +1,28 @@
-import { configure } from "enzyme";
+import React from "react";
+import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import App from "./App";
+import Character from "./Character";
 
 configure({
   adapter: new Adapter()
 });
 
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<App />);
+});
+
 describe("<App />", () => {
-  it(`should render 3 light characters`, () => {});
+  it(`should render 3 light characters`, () => {
+    expect(wrapper.find(Character)).toHaveLength(3);
+  });
+
+  it(`should render 2 dark characters`, () => {
+    wrapper.setProps({
+      side: "dark"
+    });
+    expect(wrapper.find(Character)).toHaveLength(2);
+  });
 });
