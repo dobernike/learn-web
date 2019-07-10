0e4a26a3c998c015a1fceed46c9ac7ff0dd8093b
dobernike <dobernike@mail.ru>
2019-07-10 20:54:43 +0300

    Создание приложения

--- practice/react/theory-hoc/src/index.js
+++ practice/react/theory-hoc/src/index.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  state = {
+    starWarsChars: [
+      { name: "Дарт Вэйдер", side: "dark" },
+      { name: "Люк Скайворкер", side: "light" },
+      { name: "Палпатин", side: "dark" },
+      { name: "Обиван Кеноби", side: "light" }
+    ]
+  };
+
+  render() {
+    return (
+      <ul>
+        {this.state.starWarsChars.map((char, index) => {
+          return (
+            <li key={char.name + index}>
+              <strong>{char.name}</strong> - &nbsp;
+              {char.side}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
