5c1e3bb582a71adbe650d9e91172e3276c4b0c3b
dobernike <dobernike@mail.ru>
2019-07-12 23:07:29 +0300

    Оптимизация приложения

--- practice/react/theory-hoc/src/index.js
+++ practice/react/theory-hoc/src/index.js
@@ -3,33 +3,33 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-class App extends React.Component {
-  state = {
-    starWarsChars: [
-      { name: "Дарт Вэйдер", side: "dark" },
-      { name: "Люк Скайворкер", side: "light" },
-      { name: "Палпатин", side: "dark" },
-      { name: "Обиван Кеноби", side: "light" }
-    ]
-  };
+const starWarsChars = [
+  { name: "Дарт Вэйдер", side: "dark" },
+  { name: "Люк Скайворкер", side: "light" },
+  { name: "Палпатин", side: "dark" },
+  { name: "Обиван Кеноби", side: "light" }
+];
 
-  render() {
-    return (
-      <ul>
-        {this.state.starWarsChars.map((char, index) => {
-          return (
-            <li key={char.name + index}>
-              <strong>{char.name}</strong> - &nbsp;
-              {char.side}
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
-}
+const App = ({ list, side }) => {
+  const filtredList = list.filter(char => char.side === side);
+  return (
+    <ul>
+      {filtredList.map((char, index) => {
+        return (
+          <li key={char.name + index}>
+            <strong>{char.name}</strong> - &nbsp;
+            {char.side}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App list={starWarsChars} side="light" />,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
