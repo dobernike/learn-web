31da14cd76d95f49bb0b83c50d4a21e44627b279
dobernike <dobernike@mail.ru>
2019-08-03 15:47:33 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Основы JavaScript/Типы данных/Шаблонные строки/index.js
@@ -0,0 +1,14 @@
+/*
+Шаблонные строки
+важность: 5
+
+Что выведет этот скрипт?
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ? hello 1
+
+alert( `hello ${"name"}` ); // ? hello name
+
+alert( `hello ${name}` ); // ?  hello Ilya
+*/
