264e5cd24ac6afcdd15ed0e6a74da5895910e5fe
dobernike <dobernike@mail.ru>
2019-08-03 16:18:51 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Основы JavaScript/Преобразование типов/index.js
@@ -0,0 +1,23 @@
+/*
+Преобразование типов
+важность: 5
+
+Какой результат будет у выражений ниже?
+
+"" + 1 + 0    // string 10
+"" - 1 + 0    // number -1
+true + false  // number 1
+6 / "3"       // number 2
+"2" * "3"     // number 6
+4 + 5 + "px"  // string 9px
+"$" + 4 + 5   // string $45
+"4" - 2       // number 2 
+"4px" - 2     // NaN    NaN
+7 / 0         // infinity infinity
+"  -9  " + 5  // string " -9 5"
+"  -9  " - 5  // number -14
+null + 1      // number 1
+undefined + 1 // NaN  NaN
+
+Подумайте как следует. Когда закончите – сверьтесь с решением.
+*/
