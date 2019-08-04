4beef949866a93543ad864a028119bba255c4137
dobernike <dobernike@mail.ru>
2019-08-04 23:56:06 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Качество кода/Автоматическое тестирование c использованием фреймворка Mocha/index.js
@@ -0,0 +1,23 @@
+/**
+ * Что не так с этим тестом?
+важность: 5
+
+Что не так в нижеприведённом тесте функции pow?
+
+it("Возводит x в степень n", function() {
+  let x = 5;
+
+  let result = x;
+  assert.equal(pow(x, 1), result);
+
+  result *= x;
+  assert.equal(pow(x, 2), result);
+
+  result *= x;
+  assert.equal(pow(x, 3), result);
+});
+
+P.S. Тест не содержит синтаксических ошибок и успешно проходит.
+ */
+
+// 1 it
