124e084246e0b53d9ce4eb0142b1f5b4bbf0cab0
dobernike <dobernike@mail.ru>
2019-08-06 23:49:30 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Типы данных/Методы массивов/Трансформировать в массив имён/index.js
@@ -0,0 +1,28 @@
+/**
+ * Трансформировать в массив имён
+важность: 5
+
+У вас есть массив объектов user, и в каждом из них есть user.name. Напишите код, который преобразует их в массив имён.
+
+Например:
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = ... ваш код 
+
+alert( names ); // Вася, Петя, Маша
+ */
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = users.map(item => item.name);
+
+console.log( names ); // Вася, Петя, Маша
