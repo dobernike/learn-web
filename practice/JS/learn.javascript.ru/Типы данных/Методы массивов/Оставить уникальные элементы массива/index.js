407681761d2456ddb6b34079425c42f41444d9c9
dobernike <dobernike@mail.ru>
2019-08-06 23:48:53 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Типы данных/Методы массивов/Оставить уникальные элементы массива/index.js
@@ -0,0 +1,38 @@
+/**
+ * Оставить уникальные элементы массива
+важность: 4
+
+Пусть arr – массив строк.
+
+Напишите функцию unique(arr), которая возвращает массив, содержащий только уникальные элементы arr.
+
+Например:
+
+function unique(arr) {
+   ваш код 
+}
+
+let strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", ":-O"
+];
+
+alert( unique(strings) ); // кришна, харе, :-O
+ */
+
+function unique(arr) {
+  const result = [];
+
+  for (const item of arr) {
+    if (!result.includes(item)) {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
+
+let strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", ":-O"
+];
+
+console.log(unique(strings)); // кришна, харе, :-O
