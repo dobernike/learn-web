9b0078b1960ef9d4faeffd2b935f229ff80583d9
dobernike <dobernike@mail.ru>
2019-08-06 23:48:58 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Типы данных/Методы массивов/Отсортировать пользователей по возрасту/index.js
@@ -0,0 +1,40 @@
+/**
+ * Отсортировать пользователей по возрасту
+важность: 5
+
+Напишите функцию sortByAge(users), которая принимает массив объектов со свойством age и сортирует их по нему.
+
+Например:
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let arr = [ vasya, petya, masha ];
+
+sortByAge(arr);
+
+// теперь: [vasya, masha, petya]
+alert(arr[0].name); // Вася
+alert(arr[1].name); // Маша
+alert(arr[2].name); // Петя
+ */
+
+function sortByAge(users) {
+  return users.sort((a, b) => a.age - b.age);
+};
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let arr = [vasya, petya, masha];
+
+sortByAge(arr);
+
+
+
+// теперь: [vasya, masha, petya]
+console.log(arr[0].name); // Вася
+console.log(arr[1].name); // Маша
+console.log(arr[2].name); // Петя
