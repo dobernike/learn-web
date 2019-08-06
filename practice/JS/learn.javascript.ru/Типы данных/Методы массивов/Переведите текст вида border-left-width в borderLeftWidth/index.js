d697da178c218eecfb1b0291edcba3b2a9be213d
dobernike <dobernike@mail.ru>
2019-08-06 23:49:02 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Типы данных/Методы массивов/Переведите текст вида border-left-width в borderLeftWidth/index.js
@@ -0,0 +1,27 @@
+/**
+ * Переведите текст вида border-left-width в borderLeftWidth
+важность: 5
+
+Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+
+То есть дефисы удаляются, а все слова после них получают заглавную букву.
+
+Примеры:
+
+camelize("background-color") == 'backgroundColor';
+camelize("list-style-image") == 'listStyleImage';
+camelize("-webkit-transition") == 'WebkitTransition';
+
+P.S. Подсказка: используйте split, чтобы разбить строку на массив символов, потом переделайте всё как нужно и методом join соедините обратно.
+ */
+
+function camelize(str) {
+  return str
+    .split('-')
+    .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1))
+    .join('');
+}
+
+console.log(camelize("background-color"));   //'backgroundColor';
+console.log(camelize("list-style-image"));   //'listStyleImage';
+console.log(camelize("-webkit-transition")); //'WebkitTransition';
