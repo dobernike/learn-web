02dd6eccb3fff241f79e4b305a7f320f12051f70
dobernike <Dobernike@mail.ru>
2019-08-07 10:25:48 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Продвинутая работа с функциями/Замыкание/Функция в if/index.js
@@ -0,0 +1,19 @@
+/**
+ * Функция в if
+
+Посмотрите на код. Какой будет результат у вызова на последней строке?
+
+let phrase = "Hello";
+
+if (true) {
+  let user = "John";
+
+  function sayHi() {
+    alert(`${phrase}, ${user}`);
+  }
+}
+
+sayHi();
+ */
+
+// error;
