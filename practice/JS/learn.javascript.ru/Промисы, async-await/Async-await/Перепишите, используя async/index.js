6ee587895cee3c3e81ff8427ed8272ccb7e4ffc4
dobernike <Dobernike@mail.ru>
2019-08-07 18:02:40 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Промисы, async-await/Async-await/Перепишите, используя async/index.js
@@ -0,0 +1,33 @@
+/**
+ * Перепишите, используя async/await
+
+Перепишите один из примеров раздела Цепочка промисов, используя async/await вместо .then/catch:
+
+function loadJson(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new Error(response.status);
+      }
+    })
+}
+
+loadJson('no-such-user.json') // (3)
+  .catch(alert); // Error: 404
+ */
+
+async function loadJson(url) {
+  let response = await fetch(url);
+
+  if (response.status == 200) {
+    let json = await response.json();
+    return json;
+  }
+
+  throw new Error(response.status);
+}
+
+loadJson("no-such-user.json") // (3)
+  .catch(alert); // Error: 404
