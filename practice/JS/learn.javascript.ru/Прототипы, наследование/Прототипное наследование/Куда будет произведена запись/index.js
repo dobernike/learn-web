fc3c573a824d140c332f60e4cffbd59818b79550
dobernike <Dobernike@mail.ru>
2019-08-09 17:54:24 +0300

    Добавляет решение задач

---
+++ practice/JS/learn.javascript.ru/Прототипы, наследование/Прототипное наследование/Куда будет произведена запись/index.js
@@ -0,0 +1,22 @@
+/**
+ * Куда будет произведена запись?
+важность: 5
+
+Объект rabbit наследует от объекта animal.
+
+Какой объект получит свойство full при вызове rabbit.eat(): animal или rabbit?
+
+let animal = {
+  eat() {
+    this.full = true;
+  }
+};
+
+let rabbit = {
+  __proto__: animal
+};
+
+rabbit.eat();
+ */
+
+// rabbit - до точки
