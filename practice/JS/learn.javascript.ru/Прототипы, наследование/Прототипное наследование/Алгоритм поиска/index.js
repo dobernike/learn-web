3b370af4e61202a6fe9231e9e93aaaee7178f829
dobernike <Dobernike@mail.ru>
2019-08-09 17:54:17 +0300

    Добавляет решение задач

---
+++ practice/JS/learn.javascript.ru/Прототипы, наследование/Прототипное наследование/Алгоритм поиска/index.js
@@ -0,0 +1,51 @@
+/**
+ * Алгоритм поиска
+важность: 5
+
+Задача состоит из двух частей.
+
+У нас есть объекты:
+
+let head = {
+  glasses: 1
+};
+
+let table = {
+  pen: 3
+};
+
+let bed = {
+  sheet: 1,
+  pillow: 2
+};
+
+let pockets = {
+  money: 2000
+};
+
+    С помощью свойства __proto__ задайте прототипы так, чтобы поиск любого свойства выполнялся по следующему пути: pockets → bed → table → head. Например, pockets.pen должно возвращать значение 3 (найденное в table), а bed.glasses – значение 1 (найденное в head).
+    Ответьте на вопрос: как быстрее получить значение glasses – через pockets.glasses или через head.glasses? При необходимости составьте цепочки поиска и сравните их.
+
+ */
+
+let head = {
+  glasses: 1
+};
+
+let table = {
+  __proto__: head,
+  pen: 3
+};
+
+let bed = {
+  __proto__: table,
+  sheet: 1,
+  pillow: 2
+};
+
+let pockets = {
+  __proto__: bed,
+  money: 2000
+};
+
+//одинаковое время
