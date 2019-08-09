e4f7e244d9f44d6a19d1d81a28a0390c36cdab2f
dobernike <Dobernike@mail.ru>
2019-08-09 17:54:46 +0300

    Добавляет решение задач

---
+++ practice/JS/learn.javascript.ru/Типы данных/Map и Set/Отфильтруйте анаграммы/index.js
@@ -0,0 +1,38 @@
+/**
+ * Отфильтруйте анаграммы
+важность: 4
+
+Анаграммы – это слова, у которых те же буквы в том же количестве, но они располагаются в другом порядке.
+
+Например:
+
+nap - pan
+ear - are - era
+cheaters - hectares - teachers
+
+Напишите функцию aclean(arr), которая возвращает массив слов, очищенный от анаграмм.
+
+Например:
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+alert( aclean(arr) ); // "nap,teachers,ear" or "PAN,cheaters,era"
+
+Из каждой группы анаграмм должно остаться только одно слово, не важно какое.
+ */
+
+function aclean(arr) {
+  let map = new Map();
+
+  for (let word of arr) {
+    // разбиваем слово на буквы, сортируем и объединяем снова в строку
+    let sorted = word.toLowerCase().split("").sort().join(""); // (*)
+    map.set(sorted, word);
+  }
+
+  return Array.from(map.values());
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+console.log( aclean(arr) ); // "nap,teachers,ear" or "PAN,cheaters,era"
