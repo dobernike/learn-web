aee76c97349c95982a5e56db0498d0d3ecc4c441
dobernike <Dobernike@mail.ru>
2019-08-09 17:55:55 +0300

    Добавляет решение задач

---
+++ practice/JS/learn.javascript.ru/Типы данных/Дата и время/Форматирование относительной даты/index.js
@@ -0,0 +1,58 @@
+/**
+ * Форматирование относительной даты
+важность: 4
+
+Напишите функцию formatDate(date), форматирующую date по следующему принципу:
+
+    Если спустя date прошло менее 1 секунды, вывести "прямо сейчас".
+    В противном случае, если с date прошло меньше 1 минуты, вывести "n сек. назад".
+    В противном случае, если меньше часа, вывести "m мин. назад".
+    В противном случае, полная дата в формате "DD.MM.YY HH:mm". А именно: "день.месяц.год часы:минуты", всё в виде двух цифр, т.е. 31.12.16 10:00.
+
+Например:
+
+alert( formatDate(new Date(new Date - 1)) ); // "прямо сейчас"
+
+alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 сек. назад"
+
+alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 мин. назад"
+
+// вчерашняя дата вроде 31.12.2016, 20:00
+alert( formatDate(new Date(new Date - 86400 * 1000)) );
+ */
+
+function formatDate(date) {
+  let dayOfMonth = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let diffMs = new Date() - date;
+  let diffSec = Math.round(diffMs / 1000);
+  let diffMin = diffSec / 60;
+  let diffHour = diffMin / 60;
+
+  // форматирование
+  year = year.toString().slice(-2);
+  month = month < 10 ? '0' + month : month;
+  dayOfMonth = dayOfMonth < 10 ? '0' + dayOfMonth : dayOfMonth;
+
+  if (diffSec < 1) {
+    return 'прямо сейчас';
+  } else if (diffMin < 1) {
+    return `${diffSec} сек. назад`
+  } else if (diffHour < 1) {
+    return `${diffMin} мин. назад`
+  } else {
+    return `${dayOfMonth}.${month}.${year} ${hour}:${minutes}`
+  }
+}
+
+console.log(formatDate(new Date(new Date() - 1))); // "прямо сейчас"
+
+console.log(formatDate(new Date(new Date() - 30 * 1000))); // "30 сек. назад"
+
+console.log(formatDate(new Date(new Date() - 5 * 60 * 1000))); // "5 мин. назад"
+
+// вчерашняя дата вроде 31.12.2016, 20:00
+console.log(formatDate(new Date(new Date() - 86400 * 1000)));
