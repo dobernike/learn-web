1431cf6752f83ff1d8950b7df39b11c9928413eb
dobernike <dobernike@mail.ru>
2019-08-10 21:50:56 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Прототипы, наследование/Методы прототипов, объекты без свойства __proto__/Добавьте toString в словарь/index.js
@@ -0,0 +1,52 @@
+/**
+ * Добавьте toString в словарь
+важность: 5
+
+Имеется объект dictionary, созданный с помощью Object.create(null) для хранения любых пар ключ/значение.
+
+Добавьте ему метод dictionary.toString(), который должен возвращать список ключей, разделённых запятой. Ваш toString не должен выводиться при итерации объекта с помощью цикла for..in.
+
+Вот так это должно работать:
+
+
+
+
+let dictionary = Object.create(null);
+
+// ваш код, который добавляет метод dictionary.toString
+
+// добавляем немного данных
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
+
+// только apple и __proto__ выведены в цикле
+for(let key in dictionary) {
+  alert(key); // "apple", затем "__proto__"
+}
+
+// ваш метод toString в действии
+alert(dictionary); // "apple,__proto__"
+ */
+
+let dictionary = Object.create(null, {
+  toString: {
+    value() {
+      return Object.keys(this).join();
+    }
+  }
+});
+
+// ваш код, который добавляет метод dictionary.toString
+
+
+// добавляем немного данных
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
+
+// только apple и __proto__ выведены в цикле
+for (let key in dictionary) {
+  console.log(key); // "apple", затем "__proto__"
+}
+
+// ваш метод toString в действии
+console.log(dictionary.toString()); // "apple,__proto__"
