e89eec280cc18072ae8117545390b4f510c7acdd
dobernike <dobernike@mail.ru>
2019-08-10 21:50:47 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Прототипы, наследование/Встроенные прототипы/Добавить функциям метод f.defer(ms)/index.js
@@ -0,0 +1,25 @@
+/**
+ * Добавить функциям метод "f.defer(ms)"
+важность: 5
+
+Добавьте всем функциям в прототип метод defer(ms), который вызывает функции через ms миллисекунд.
+
+После этого должен работать такой код:
+
+function f() {
+  alert("Hello!");
+}
+
+f.defer(1000); // выведет "Hello!" через 1 секунду
+ */
+
+Function.prototype.defer = function (ms) {
+  setTimeout(this, ms);
+}
+
+function f() {
+  console.log("Hello!");
+}
+
+f.defer(1000); // выведет "Hello!" через 1 секунду
+
