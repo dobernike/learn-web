3ab1f85b7f0bf209f4a7abace6947e5600bcdfc7
dobernike <dobernike@mail.ru>
2019-08-11 23:00:47 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Обработка ошибок/Пользовательские ошибки, расширение Error/index.js
@@ -0,0 +1,26 @@
+/**
+ * Наследование от SyntaxError
+важность: 5
+
+Создайте класс FormatError, который наследует от встроенного класса SyntaxError.
+
+Класс должен поддерживать свойства message, name и stack.
+
+Пример использования:
+
+let err = new FormatError("ошибка форматирования");
+
+alert( err.message ); // ошибка форматирования
+alert( err.name ); // FormatError
+alert( err.stack ); // stack
+
+alert( err instanceof FormatError ); // true
+alert( err instanceof SyntaxError ); // true (потому что наследует от SyntaxError)
+ */
+
+class FormatError extends SyntaxError {
+  constructor(message) {
+    super(message);
+    this.name = "FormatError";
+  }
+}
