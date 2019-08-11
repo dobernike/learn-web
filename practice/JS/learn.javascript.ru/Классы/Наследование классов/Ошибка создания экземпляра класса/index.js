426a38b7aecb86f44ab0a9a2c94f594101d3035a
dobernike <dobernike@mail.ru>
2019-08-11 21:19:40 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Классы/Наследование классов/Ошибка создания экземпляра класса/index.js
@@ -0,0 +1,46 @@
+/**
+ * Ошибка создания экземпляра класса
+важность: 5
+
+В коде ниже класс Rabbit наследует Animal.
+
+К сожалению, объект класса Rabbit не создаётся. Что не так? Исправьте ошибку.
+
+class Animal {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+    this.name = name;
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("Белый кролик"); // Error: this is not defined
+alert(rabbit.name);
+ */
+
+class Animal {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+    super(name);
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("Белый кролик"); // Error: this is not defined
+console.log(rabbit.name);
+
+// Нет super() в конструкторе
