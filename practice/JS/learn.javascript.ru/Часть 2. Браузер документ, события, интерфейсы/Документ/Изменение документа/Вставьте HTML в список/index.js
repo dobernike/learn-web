3da12f5902b78c806cf621c8299a515a3267437e
dobernike <dobernike@mail.ru>
2019-08-14 21:35:52 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Часть 2. Браузер документ, события, интерфейсы/Документ/Изменение документа/Вставьте HTML в список/index.js
@@ -0,0 +1,13 @@
+/**
+ * Вставьте HTML в список
+важность: 5
+
+Напишите код для вставки <li>2</li><li>3</li> между этими двумя <li>:
+
+<ul id="ul">
+  <li id="one">1</li>
+  <li id="two">4</li>
+</ul>
+ */
+
+ one.insertAdjacentHTML('afterend', '<li>2</li><li>3</li>');
