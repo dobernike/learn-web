c2e83fe9159fa54e8a544f238ac02107341ecd5c
dobernike <dobernike@mail.ru>
2019-08-14 13:28:52 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Часть 2. Браузер документ, события, интерфейсы/Документ/Навигация по DOM-элементам/Вопрос о соседях/index.js
@@ -0,0 +1,13 @@
+/**
+ * Вопрос о соседях
+важность: 5
+
+Если elem – произвольный узел DOM-элемента…
+
+    Правда, что elem.lastChild.nextSibling всегда равен null?
+    Правда, что elem.children[0].previousSibling всегда равен null ?
+
+ */
+
+ // null
+ // нет
