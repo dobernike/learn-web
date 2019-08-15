45c0590ebf8efc064a9708bab1d45c6832937166
dobernike <Dobernike@mail.ru>
2019-08-15 17:03:04 +0300

    Добавляет решение задачи

---
+++ practice/JS/learn.javascript.ru/Часть 2. Браузер документ, события, интерфейсы/Введение в события/Введение в браузерные события/Скрыть элемент по нажатию кнопки/index.js
@@ -0,0 +1,8 @@
+/**Скрыть элемент по нажатию кнопки
+важность: 5
+
+Добавьте JavaScript к кнопке button, чтобы при нажатии элемент <div id="text"> исчезал. */
+
+hider.onclick = function() {
+  text.hidden = true;
+};
