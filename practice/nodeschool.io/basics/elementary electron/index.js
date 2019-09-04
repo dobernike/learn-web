0cbd80e12a53f806a8681b61c689f96971f58978
dobernike <dobernike@mail.ru>
2019-09-04 14:35:47 +0300

    Добавляет решение задачи

--- practice/nodeschool.io/basics/elementary electron/index.js
+++ practice/nodeschool.io/basics/elementary electron/index.js
@@ -1,1 +1,25 @@
-require('cat-picture');
+var picture = require('cat-picture')
+var src = picture.src;
+
+picture.remove();
+
+var image = require('lightning-image-poly')
+var viz = new image('#visualization', null, [src], { hullAlgorithm: 'convex' })
+
+var remote = require('electron').remote
+var fs = require('fs')
+
+function save () {
+  remote.getCurrentWebContents().printToPDF({
+    portrait: true
+  }, function (err, data) {
+    fs.writeFile('annotation.pdf', data, function (err) {
+      if (err) alert('error generating pdf! ' + err.message)
+      else alert('pdf saved!')
+    })
+  })
+}
+
+window.addEventListener('keydown', function (e) {
+  if (e.keyCode == 80) save()
+})
