4b99f6a9abf0212118d03d8430272dfc6786a555
dobernike <dobernike@mail.ru>
2019-09-04 14:10:58 +0300

    Добавляет решение Cat Picture

--- practice/nodeschool.io/basics/elementary electron/app.js
+++ practice/nodeschool.io/basics/elementary electron/app.js
@@ -1,8 +1,6 @@
 var electron = require('electron');
 
-const url = 'file://' + __dirname + '/index.html';
-
 electron.app.on('ready', function () {
   var mainWindow = new electron.BrowserWindow({ width: 600, height: 800 });
-  mainWindow.loadURL(url);
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 });
