9e452203d2a5c245f695f0f6cfbfcd6126b5b8dd
dobernike <Dobernike@mail.ru>
2019-12-04 23:48:52 +0300

    react 15:00

---
+++ practice/react/typescriptreact/src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Navbar: React.FC = () => (
+  <nav>
+    <div className="nav-wrapper purple darken-2">
+      <a href="#" className="brand-logo">
+        React + TS
+      </a>
+      <ul className="right hide-on-med-and-down">
+        <li>
+          <a href="/">Список дел</a>
+        </li>
+        <li>
+          <a href="/">Информация</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
