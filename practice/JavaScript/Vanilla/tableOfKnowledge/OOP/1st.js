91c484348f06bb0c5f413483a1aa08143d8147fb
Paul <dobernike@mail.ru>
2019-12-17 09:35:06 +0300

    finale

--- practice/JavaScript/Vanilla/tableOfKnowledge/OOP/1st.js
+++ practice/JavaScript/Vanilla/tableOfKnowledge/OOP/1st.js
@@ -20,3 +20,34 @@ function Person(name) {
 
 var person1 = new Person("Bob");
 var person2 = new Person("Sarah");
+
+function Person(first, last, age, gender, interests) {
+  this.name = {
+    first: first,
+    last: last
+  };
+  this.age = age;
+  this.gender = gender;
+  this.interests = interests;
+  this.bio = function() {
+    alert(
+      this.name.first +
+        " " +
+        this.name.last +
+        " is " +
+        this.age +
+        " years old. He likes " +
+        this.interests[0] +
+        this.interests[1] +
+        "."
+    );
+  };
+  this.greeting = function() {
+    alert("Hi! I`m " + this.name.first + ".");
+  };
+}
+
+var person1 = new Person("Bob", "Smith", 32, "male", ["music", "skiing"]);
+person1["age"];
+person1.interests[1];
+person1.bio();
