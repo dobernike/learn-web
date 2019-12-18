6e31fc75b6159550a87a3aefe0fdb543eb581525
Paul <dobernike@mail.ru>
2019-12-18 18:07:04 +0300

    Добавляет свой

--- practice/JavaScript/React/react-tables-discovery/src/App.tsx
+++ practice/JavaScript/React/react-tables-discovery/src/App.tsx
@@ -1,8 +1,34 @@
 import React from "react";
-import ReactDatasheet from "./ReactDatasheet";
+import MyOwnSheet from "./ReactDataSheet/MyOwnSheet";
+import ReactDatasheet from "./ReactDataSheet/BasicTypescript";
+import BasicSheet from "./ReactDataSheet/BasicSheet";
+import ComponentSheet from "./ReactDataSheet/ComponentSheet";
+import MathSheet from "./ReactDataSheet/MathSheet";
+import OverrideEverythingSheet from "./ReactDataSheet/OverrideEverythingSheet";
 
 const App: React.FC = () => {
-  return <ReactDatasheet />;
+  return (
+    <>
+      <section>
+        <h1 style={{ textAlign: "center" }}>ReactDataSheet</h1>
+        <hr />
+        <h2 style={{ textAlign: "center" }}>test</h2>
+        <hr />
+
+        <MyOwnSheet />
+
+        <hr />
+        <h2 style={{ textAlign: "center" }}>exaples</h2>
+        <hr />
+
+        <ReactDatasheet />
+        <BasicSheet />
+        <ComponentSheet />
+        <MathSheet />
+        <OverrideEverythingSheet />
+      </section>
+    </>
+  );
 };
 
 export default App;
---
+++ practice/JavaScript/React/react-tables-discovery/src/ReactDataSheet/BasicSheet.js
@@ -0,0 +1,66 @@
+import _ from "lodash";
+import React from "react";
+import ReactDataSheet from "react-datasheet";
+
+export default class BasicSheet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: [
+        [
+          { readOnly: true, value: "" },
+          { value: "A", readOnly: true },
+          { value: "B", readOnly: true },
+          { value: "C", readOnly: true },
+          { value: "D", readOnly: true }
+        ],
+        [
+          { readOnly: true, value: 1 },
+          { value: 1 },
+          { value: 3 },
+          { value: 3 },
+          { value: 3 }
+        ],
+        [
+          { readOnly: true, value: 2 },
+          { value: 2 },
+          { value: 4 },
+          { value: 4 },
+          { value: 4 }
+        ],
+        [
+          { readOnly: true, value: 3 },
+          { value: 1 },
+          { value: 3 },
+          { value: 3 },
+          { value: 3 }
+        ],
+        [
+          { readOnly: true, value: 4 },
+          { value: 2 },
+          { value: 4 },
+          { value: 4 },
+          { value: 4 }
+        ]
+      ]
+    };
+  }
+  render() {
+    return (
+      <ReactDataSheet
+        data={this.state.grid}
+        valueRenderer={cell => cell.value}
+        onContextMenu={(e, cell, i, j) =>
+          cell.readOnly ? e.preventDefault() : null
+        }
+        onCellsChanged={changes => {
+          const grid = this.state.grid.map(row => [...row]);
+          changes.forEach(({ cell, row, col, value }) => {
+            grid[row][col] = { ...grid[row][col], value };
+          });
+          this.setState({ grid });
+        }}
+      />
+    );
+  }
+}
