c45397d1533d663183eedc781ec139478bcef935
Paul <dobernike@mail.ru>
2020-01-13 15:06:55 +0300

    Added rx chat practice

--- practice/JavaScript/React/rxjs_react_chat/src/App.js
+++ practice/JavaScript/React/rxjs_react_chat/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "./index.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { FirstPerson, SecondPerson, PersonSwitcher } from "./components";
+import "./App.css";
 
-const App = () => <BrowserRouter />;
+const App = () => (
+  <Router>
+    <>
+      <PersonSwitcher />
+      <Switch>
+        <Route path="/" component={FirstPerson} exact />>
+        <Route path="/first-person" component={FirstPerson} exact />>
+        <Route path="/second-person" component={SecondPerson} exact />>
+      </Switch>
+    </>
+  </Router>
+);
 
 export default App;
