e9bdbd87f45e99441c9f8c4628e07bbd86723865
Paul <dobernike@mail.ru>
2020-01-17 18:01:58 +0300

    change data

--- practice/JavaScript/React/react-tables-discovery/src/ReactDataSheet/MyOwn/mockData.js
+++ practice/JavaScript/React/react-tables-discovery/src/ReactDataSheet/MyOwn/mockData.js
@@ -130,6 +130,13 @@ export const table1 = {
     expr: ""
   },
   "07": { key: "7", value: "Комиссионные доходы", readOnly: true, expr: "" },
+  "08": {
+    key: "8",
+    value: "Итого от инвестиционной деятельности",
+    readOnly: true,
+    expr: ""
+  },
+
   A0: { key: "A0", value: "0.00", readOnly: true, expr: "=A1+A2+A3+A4+A5" },
   A1: { key: "A1", value: "0.00", expr: "" },
   A2: {
@@ -142,6 +149,7 @@ export const table1 = {
   A5: { key: "A5", value: "0.00", expr: "" },
   A6: { key: "A6", value: "0.00", expr: "" },
   A7: { key: "A7", value: "0.00", expr: "" },
+  A8: { key: "A8", value: "0.00", readOnly: true, expr: "=A0" },
 
   B0: { key: "B0", value: "0.00", readOnly: true, expr: "" },
   B1: { key: "B1", value: "0.00", expr: "" },
@@ -151,6 +159,7 @@ export const table1 = {
   B5: { key: "B5", value: "0.00", expr: "" },
   B6: { key: "B6", value: "0.00", expr: "" },
   B7: { key: "B7", value: "0.00", expr: "" },
+  B8: { key: "B8", value: "0.00", readOnly: true, expr: "=B0" },
 
   C0: { key: "C0", value: "0.00", readOnly: true, expr: "" },
   C1: { key: "C1", value: "0.00", expr: "" },
@@ -160,6 +169,7 @@ export const table1 = {
   C5: { key: "C5", value: "0.00", expr: "" },
   C6: { key: "C6", value: "0.00", expr: "" },
   C7: { key: "C7", value: "0.00", expr: "" },
+  C8: { key: "C8", value: "0.00", readOnly: true, expr: "=C0" },
 
   D0: { key: "D0", value: "", readOnly: true, expr: "" },
   D1: { key: "D1", value: "-", expr: "" },
@@ -168,5 +178,6 @@ export const table1 = {
   D4: { key: "D4", value: "-", expr: "" },
   D5: { key: "D5", value: "-", expr: "" },
   D6: { key: "D6", value: "-", expr: "" },
-  D7: { key: "D7", value: "-", expr: "" }
+  D7: { key: "D7", value: "-", expr: "" },
+  D8: { key: "D8", value: "", readOnly: true, expr: "" }
 };
