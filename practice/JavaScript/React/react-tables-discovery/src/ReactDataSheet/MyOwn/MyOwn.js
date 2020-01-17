c78f90dfc6753bb80f526d675286cb07ff9fdac9
Paul <dobernike@mail.ru>
2020-01-17 18:02:07 +0300

    practice with table

--- practice/JavaScript/React/react-tables-discovery/src/ReactDataSheet/MyOwn/MyOwn.js
+++ practice/JavaScript/React/react-tables-discovery/src/ReactDataSheet/MyOwn/MyOwn.js
@@ -5,9 +5,6 @@ import { table1, header } from "./mockData";
 export default () => {
   return (
     <article style={{ padding: "0 5%", backgroundColor: "#F8F8F8" }}>
-      <Table data={header} />
-      <Table data={table1} />
-      <Table data={table1} />
       <Table data={table1} />
     </article>
   );
