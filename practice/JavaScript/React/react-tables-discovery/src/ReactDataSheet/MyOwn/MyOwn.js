89e117ab8065c06e7fa14fb82478d92c2ea07c4f
Paul <dobernike@mail.ru>
2020-01-20 17:52:17 +0300

    Добавляет отображение table на div

--- practice/JavaScript/React/react-tables-discovery/src/ReactDataSheet/MyOwn/MyOwn.js
+++ practice/JavaScript/React/react-tables-discovery/src/ReactDataSheet/MyOwn/MyOwn.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Table from "./table1";
-import { table1, header } from "./mockData";
+// import Table from "./table1";
+import TableOver from "./MyOver";
+import { table2 } from "./mockData";
 
 export default () => {
   return (
     <article style={{ padding: "0 5%", backgroundColor: "#F8F8F8" }}>
-      <Table data={table1} />
+      {/* <Table data={table1} /> */}
+      {/* <hr /> */}
+      <TableOver data={table2} />
     </article>
   );
 };
