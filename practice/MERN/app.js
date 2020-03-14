34d8e483359b3aa5091b9702f9a30c07d09be19a
Paul <dobernike@mail.ru>
2020-03-14 15:53:56 +0300

    end of mern

--- practice/MERN/app.js
+++ practice/MERN/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const config = require("config");
+const path = require("path");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -9,6 +10,14 @@ app.use(express.json({ extended: true }));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/link", require("./routes/link.routes"));
 app.use("/t", require("./routes/redirect.routes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = config.get("port") || 5000;
 
