8868234901e89cc91d18c0258028a6b72f665fb9
Paul <dobernike@mail.ru>
2020-03-17 16:24:49 +0300

    Добавляет практику с useEffect

--- practice/JavaScript/React/other/hooks/src/App.js
+++ practice/JavaScript/React/other/hooks/src/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TodoList from "./TodoList";
 
 export default function App() {
-  // state = {
-  // todos: [
-  //   {id: 1, title: 'First todo', completed: false},
-  //   {id: 2, title: 'Second todo', completed: true},
-  // ]
-  // }
-  const [todos, setTodos] = useState([
-    { id: 1, title: "First todo", completed: false },
-    { id: 2, title: "Second todo", completed: true }
-  ]);
-
+  const [todos, setTodos] = useState([]);
   const [todoTitle, setTodoTitle] = useState();
+
+  const handleClick = () => console.log("");
+
+  useEffect(() => {
+    const raw = localStorage.getItem("todos") || [];
+    setTodos(JSON.parse(raw));
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [todos]);
 
   const addTodo = event => {
     if (event.key === "Enter") {
