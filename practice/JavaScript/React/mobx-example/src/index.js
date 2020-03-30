60d19a9f7ff9ba1e891d3a1a9948652fa87563cd
dobernike <Dobernike@mail.ru>
2020-03-30 09:12:27 +0300

    Наблюдаемое и наблюдатель

--- practice/JavaScript/React/mobx-example/src/index.js
+++ practice/JavaScript/React/mobx-example/src/index.js
@@ -2,12 +2,36 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
 
-class App extends Component {
+const counterState = observable({
+  count: 0
+});
+
+counterState.increment = function() {
+  this.count++;
+};
+
+counterState.decrement = function() {
+  this.count--;
+};
+
+@observer
+class Counter extends Component {
+  handleIncrement = () => {
+    this.props.store.increment();
+  };
+  handleDecrement = () => {
+    this.props.store.decrement();
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>Hello world!</h1>
+        <h1>{this.props.store.count}</h1>
+        <button onClick={this.handleDecrement}>-1</button>
+        <button onClick={this.handleIncrement}>+1</button>
       </div>
     );
   }
@@ -15,7 +39,7 @@ class App extends Component {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter store={counterState} />
   </React.StrictMode>,
   document.getElementById("root")
 );
