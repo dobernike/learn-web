7c975db74d891ecf4c1e0efb9333d33c50378e0d
dobernike <Dobernike@mail.ru>
2020-04-07 21:50:51 +0300

    8

--- practice/GraphQL/Apollo/server/app.js
+++ practice/GraphQL/Apollo/server/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+  })
+);
 
 app.listen(4000, () => {
   console.log("list 4000");
