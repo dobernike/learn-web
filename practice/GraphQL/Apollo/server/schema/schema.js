c22a0cde92227e7d2b79c0059fa183b8fa49eb2a
dobernike <Dobernike@mail.ru>
2020-04-08 20:07:57 +0300

    15

--- practice/GraphQL/Apollo/server/schema/schema.js
+++ practice/GraphQL/Apollo/server/schema/schema.js
@@ -74,6 +74,18 @@ const RootQuery = new GraphQLObjectType({
         return _.find(authors, { id: args.id });
       },
     },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      },
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
+      },
+    },
   },
 });
 
