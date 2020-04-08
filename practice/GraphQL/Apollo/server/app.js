f44134c33c114c800974fc6ac1b5500a8f3d9ff8
dobernike <Dobernike@mail.ru>
2020-04-08 22:50:33 +0300

    16

--- practice/GraphQL/Apollo/server/app.js
+++ practice/GraphQL/Apollo/server/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://paul:123@cluster0-th9bd.mongodb.net/test?retryWrites=true&w=majority"
+);
+mongoose.connection.once("open", () => {
+  console.log("connect to db");
+});
 
 app.use(
   "/graphql",
