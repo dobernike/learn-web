4665b89aa0b46099cbeee730403b94447b640d84
dobernike <Dobernike@mail.ru>
2020-04-08 23:05:07 +0300

    18

--- practice/GraphQL/Apollo/server/schema/schema.js
+++ practice/GraphQL/Apollo/server/schema/schema.js
@@ -91,6 +91,27 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        let author = new Author({
+          name: args.name,
+          age: args.age,
+        });
+        return author.save();
+      },
+    },
+  },
+});
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation: Mutation,
 });
