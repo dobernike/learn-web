5672be201481693213a392c894a2f29cde05a3f5
dobernike <Dobernike@mail.ru>
2020-04-08 23:24:40 +0300

    19

--- practice/GraphQL/Apollo/server/models/book.js
+++ practice/GraphQL/Apollo/server/models/book.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   name: String,
   genre: String,
-  authoId: String,
+  authorId: String,
 });
 
 module.exports = mongoose.model("Book", bookSchema);
