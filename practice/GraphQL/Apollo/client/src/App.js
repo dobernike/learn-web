8eb42c92d327c48139888b03d7909a3c23d7d261
dobernike <Dobernike@mail.ru>
2020-04-09 20:42:08 +0300

    Add component

--- practice/GraphQL/Apollo/client/src/App.js
+++ practice/GraphQL/Apollo/client/src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Ninja`s Reading List</h1>
-    </div>
-  );
+//components
+import BookList from "./components/BookList";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Ninja`s Reading List</h1>
+        <BookList />
+      </div>
+    );
+  }
 }
 
 export default App;
