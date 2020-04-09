efb866cba52a0c7bde1adb3dc1bc533612b5aa95
dobernike <Dobernike@mail.ru>
2020-04-09 21:32:27 +0300

    27

--- practice/GraphQL/Apollo/client/src/components/BookList.js
+++ practice/GraphQL/Apollo/client/src/components/BookList.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
 
 class BookList extends Component {
+  displayBooks() {
+    const data = this.props.data;
+
+    if (data.loading) {
+      return <div>Loading books...</div>;
+    }
+
+    return data.books.map((book) => <li key={book.id}>{book.name}</li>);
+  }
+
   render() {
     return (
       <div>
-        <ul id="book-list">
-          <li>Book name</li>
-        </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
       </div>
     );
   }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
--- practice/GraphQL/Apollo/server/app.js
+++ practice/GraphQL/Apollo/server/app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://paul:123@cluster0-th9bd.mongodb.net/test?retryWrites=true&w=majority"
