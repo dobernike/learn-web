00b8b087b7e8d0efd45be1578080d3361676afc8
dobernike <Dobernike@mail.ru>
2020-04-10 21:08:22 +0300

    29

---
+++ practice/GraphQL/Apollo/client/src/queries/queries.js
@@ -0,0 +1,21 @@
+import { gql } from "apollo-boost";
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+const getAuthorsQuery = gql`
+  {
+    authors {
+      name
+      id
+    }
+  }
+`;
+
+export { getAuthorsQuery, getBooksQuery };
