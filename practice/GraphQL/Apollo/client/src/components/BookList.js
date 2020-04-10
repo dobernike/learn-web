84f18d340ca750d3445ed53fc3f91504a1be5bfa
dobernike <Dobernike@mail.ru>
2020-04-10 22:03:34 +0300

    35

--- practice/GraphQL/Apollo/client/src/components/BookList.js
+++ practice/GraphQL/Apollo/client/src/components/BookList.js
@@ -6,6 +6,12 @@ import { getBooksQuery } from "../queries/queries";
 import BookDetails from "./BookDetails";
 
 class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null,
+    };
+  }
   displayBooks() {
     const data = this.props.data;
 
@@ -13,14 +19,23 @@ class BookList extends Component {
       return <div>Loading books...</div>;
     }
 
-    return data.books.map((book) => <li key={book.id}>{book.name}</li>);
+    return data.books.map((book) => (
+      <li
+        key={book.id}
+        onClick={(e) => {
+          this.setState({ selected: book.id });
+        }}
+      >
+        {book.name}
+      </li>
+    ));
   }
 
   render() {
     return (
       <div>
         <ul id="book-list">{this.displayBooks()}</ul>
-        <BookDetails />
+        <BookDetails bookId={this.state.selected} />
       </div>
     );
   }
