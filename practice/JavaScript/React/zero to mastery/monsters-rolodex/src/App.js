f8673a2029207002ac479ea316e4d45ab89380c5
dobernike <Dobernike@mail.ru>
2020-04-15 00:05:36 +0300

    Update

--- practice/JavaScript/React/zero to mastery/monsters-rolodex/src/App.js
+++ practice/JavaScript/React/zero to mastery/monsters-rolodex/src/App.js
@@ -20,11 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList namme="Yihua!">
-          {this.state.monsters.map((monster) => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))}
-        </CardList>
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
