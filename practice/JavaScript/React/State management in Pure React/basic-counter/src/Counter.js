289303f355952449be71aa863564409b2d4386da
dobernike <Dobernike@mail.ru>
2020-04-14 23:02:14 +0300

    Update

--- practice/JavaScript/React/State management in Pure React/basic-counter/src/Counter.js
+++ practice/JavaScript/React/State management in Pure React/basic-counter/src/Counter.js
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from "react";
 
-const getStateFromLocalStorage = () => {
-  const storage = localStorage.getItem("counterState");
-  if (storage) return JSON.parse(storage).count;
-  return 0;
-};
-
 const storeStateInLocalStorage = (count) => {
   localStorage.setItem("counterState", JSON.stringify({ count }));
 };
 
 document.title = "Hello";
 
+const useLocalStorage = (initialState, key) => {
+  const get = () => {
+    const storage = localStorage.getItem(key);
+    if (storage) return JSON.parse(storage)[value];
+    return initialState;
+  };
+
+  const [value, setValue] = useState(get());
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify({ value }));
+  }, [value]);
+
+  return [value, setValue];
+};
+
 const Counter = ({ max, step }) => {
-  const [count, setCount] = useState(getStateFromLocalStorage);
+  const [count, setCount] = useLocalStorage(0, "count");
 
   const increment = () => {
     setCount((c) => {
