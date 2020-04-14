2d4e931025d36dd21ee003f17daf0604c3562344
dobernike <Dobernike@mail.ru>
2020-04-14 23:41:55 +0300

    Update

--- practice/JavaScript/React/zero to mastery/monsters-rolodex/src/App.js
+++ practice/JavaScript/React/zero to mastery/monsters-rolodex/src/App.js
@@ -22,6 +22,13 @@ class App extends Component {
       ],
     };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div>
