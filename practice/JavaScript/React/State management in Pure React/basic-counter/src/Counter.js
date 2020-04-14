be389a9a0d1f060ec801d00b7063b033ffb334a7
dobernike <Dobernike@mail.ru>
2020-04-14 21:37:04 +0300

    update

--- practice/JavaScript/React/State management in Pure React/basic-counter/src/Counter.js
+++ practice/JavaScript/React/State management in Pure React/basic-counter/src/Counter.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 3,
+    };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+  reset() {
+    this.setState({ count: 0 });
+  }
+
   render() {
+    const { count } = this.state;
     return (
       <section className="Counter">
-        <h1>Count: {0}</h1>
-        <button onClick={() => {}} className="full-width">Increment</button>
-        <button onClick={() => {}} className="full-width">Decrement</button>
-        <button onClick={() => {}} className="full-width">Reset</button>
+        <h1>Count: {count}</h1>
+        <button onClick={this.increment} className="full-width">
+          Increment
+        </button>
+        <button onClick={this.decrement} className="full-width">
+          Decrement
+        </button>
+        <button onClick={this.reset} className="full-width">
+          Reset
+        </button>
       </section>
     );
   }
---
+++ practice/JavaScript/React/State management in Pure React/readme.md
@@ -0,0 +1,1 @@
+sbs https://gist.github.com/stevekinney/7b25237edd43c3e450b288c9e7857c29
