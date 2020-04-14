cb9d74f8b515f09297871f7894df1877282de8d1
dobernike <Dobernike@mail.ru>
2020-04-14 23:38:47 +0300

    Update

--- practice/JavaScript/React/zero to mastery/monsters-rolodex/src/App.js
+++ practice/JavaScript/React/zero to mastery/monsters-rolodex/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Frankenstein",
+          id: "asc1",
+        },
+        {
+          name: "Dracula",
+          id: "fea2",
+        },
+        {
+          name: "Zombie",
+          id: "gfr3",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
