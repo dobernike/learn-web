75a3d14224b9b0a4d87203f6064bf42a7b3441fe
dobernike <Dobernike@mail.ru>
2020-04-15 16:41:02 +0300

    Routing In Our Project

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/App.js
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
