7117c2c029a4ba7bccfd29bca2f4e569d8f26b55
dobernike <Dobernike@mail.ru>
2020-04-15 19:10:35 +0300

    Header Component

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/App.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/App.jsx
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
