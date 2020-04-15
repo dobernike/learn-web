587480e0d0b5e7e279dfb9afd8d086a91b623952
dobernike <Dobernike@mail.ru>
2020-04-15 17:55:24 +0300

    Shop Page

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/App.js
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/App.js
@@ -2,21 +2,16 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 import "./App.css";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
