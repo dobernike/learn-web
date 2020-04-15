e23e1eb12b5e5de55f6134f3f39195761f88e64c
dobernike <Dobernike@mail.ru>
2020-04-15 08:49:54 +0300

    Update

--- practice/JavaScript/React/zero to mastery/monsters-rolodex/src/App.js
+++ practice/JavaScript/React/zero to mastery/monsters-rolodex/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -18,9 +19,18 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
