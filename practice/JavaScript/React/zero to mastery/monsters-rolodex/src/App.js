7b4ea0425676136f0ff9c1fa7a4c21c872eebeea
dobernike <Dobernike@mail.ru>
2020-04-15 09:13:22 +0300

    Update

--- practice/JavaScript/React/zero to mastery/monsters-rolodex/src/App.js
+++ practice/JavaScript/React/zero to mastery/monsters-rolodex/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -25,10 +28,9 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <input
-          type="search"
+        <SearchBox
           placeholder="search monsters"
-          onChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonsters} />
       </div>
