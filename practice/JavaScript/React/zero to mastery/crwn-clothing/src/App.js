1f53b471a027efd7e96beea083542a722112cbbb
dobernike <Dobernike@mail.ru>
2020-04-15 14:47:56 +0300

    Update

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/App.js
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import HomePage from "./homepage.component";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
