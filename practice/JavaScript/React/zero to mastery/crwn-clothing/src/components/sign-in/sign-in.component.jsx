c39bede699c9eaa78495e82e5b795bb0a24199e6
dobernike <Dobernike@mail.ru>
2020-04-15 21:39:11 +0300

    Form Input Component

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import FormInput from "../form-input/form-input.component";
+
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -31,23 +33,22 @@ class SignIn extends React.Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          onChange={this.handleChange}
-          <input
+          <FormInput
             name="email"
             type="email"
             value={this.state.email}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label="email"
             required
           />
-          <label>Email</label>
-          <input
+          <FormInput
             name="password"
             type="password"
             value={this.state.password}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label="password"
             required
           />
-          <label>Password</label>
           <input type="submit" value="Submit Form" />
         </form>
       </div>
