f786a5b149b7039d6d7b8211908ac1f6a52f281c
dobernike <Dobernike@mail.ru>
2020-04-16 19:01:38 +0300

    Improving Naming of Component

---
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => (
+  <div className="collection-page">
+    <h2>COLLECTION PAGE</h2>
+  </div>
+);
+
+export default CollectionPage;
--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/pages/shop/shop.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/pages/shop/shop.component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
-import CategoryPage from '../category/category.component';
+import CollectionPage from '../collection/collection.component';
 
 const ShopPage = ({ match }) => (
   <div className="shop-page">
     <Route exact path={`${match.path}`} component={CollectionOverview} />
-    <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
