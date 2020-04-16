df51aa11768364ec71da25a657a9a080a10b3111
dobernike <Dobernike@mail.ru>
2020-04-16 09:53:09 +0300

    Setting Up Redux

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/index.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/index.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import "./index.css";
-import App from "./App";
+import store from './redux/store';
+
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
