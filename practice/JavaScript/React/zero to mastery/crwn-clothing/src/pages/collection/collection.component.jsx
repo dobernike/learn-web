cb8200d3de681af91287244a28f5a87eb097de40
dobernike <Dobernike@mail.ru>
2020-04-16 19:34:25 +0300

    Data Normalization Collection Page

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/pages/collection/collection.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/pages/collection/collection.component.jsx
@@ -7,11 +7,19 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => (
-  <div className="collection-page">
-    <h2>COLLECTION PAGE</h2>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/redux/shop/shop.selectors.js
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/redux/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -16,8 +8,7 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   );
