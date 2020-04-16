68d5aef32a1a73a9d042c92ee6fe523bc45fa859
dobernike <Dobernike@mail.ru>
2020-04-16 20:15:49 +0300

    Stripe Integration

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/pages/checkout/checkout.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
@@ -36,6 +37,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: any future - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
