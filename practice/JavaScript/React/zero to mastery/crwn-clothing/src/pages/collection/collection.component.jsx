d45a57f7a03d6c7b8dc6039c1f0afd49bdb7664c
dobernike <Dobernike@mail.ru>
2020-04-16 19:13:25 +0300

    Collection Routing and Selector

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/pages/collection/collection.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/pages/collection/collection.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-import CollectionItem from '../../components/collection-item';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
@@ -10,4 +13,8 @@ const CollectionPage = ({ match }) => (
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
