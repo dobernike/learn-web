889d83b76f20e60aae30305bfd53c639f998c8e2
dobernike <Dobernike@mail.ru>
2020-04-16 14:22:55 +0300

    User Selectors

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
@@ -15,8 +16,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  itemCount: selectCartItemsCount(state),
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount,
 });
 
 const mapDispatchToProps = (dispatch) => ({
