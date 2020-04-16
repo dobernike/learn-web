3196aa67b86d99e358ffce54565e8b568daa1ba2
dobernike <Dobernike@mail.ru>
2020-04-16 12:20:14 +0300

    Cart Dropdown Component

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/components/header/header.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -32,6 +33,7 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
