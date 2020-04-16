ed3bafab391fdcd4d4db462cafed4c502404385e
dobernike <Dobernike@mail.ru>
2020-04-16 15:10:45 +0300

    Checkout Item Component

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/pages/checkout/checkout.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import {
   selectCartItems,
   selectCartTotal,
@@ -28,7 +30,9 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map((cartItem) => cartItem.name)}
+    {cartItems.map((cartItem) => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
