371721365f09dc9feff49da55fdc9ac5fdc523fa
dobernike <Dobernike@mail.ru>
2020-04-16 17:26:24 +0300

    Redux Persist

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/index.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/index.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
-import './index.css';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
