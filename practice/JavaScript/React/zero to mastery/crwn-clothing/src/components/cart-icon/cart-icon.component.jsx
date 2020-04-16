e10b6072e718c257e07a95e873459673201d55a1
dobernike <Dobernike@mail.ru>
2020-04-16 13:59:05 +0300

    Selectors in Redux

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -7,15 +7,22 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+    0
+  ),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
