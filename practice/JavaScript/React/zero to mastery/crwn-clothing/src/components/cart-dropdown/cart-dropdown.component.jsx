c843b1c744cc5e0e6abf73ba5c8ca32c2c59aea3
dobernike <Dobernike@mail.ru>
2020-04-16 15:00:50 +0300

    Dispatch action shorthand

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,10 +7,11 @@ import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.length ? (
@@ -21,7 +22,12 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className="empty-message">Your cart is empty</span>
       )}
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>
+    <CustomButton
+      onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden());
+      }}
+    >
       GO TO CHECKOUT
     </CustomButton>
   </div>
