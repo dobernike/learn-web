6e4aca88138747d1b3bf3fadbdd70888dde189ca
dobernike <Dobernike@mail.ru>
2020-04-17 15:42:33 +0300

    Bringing Shop Data

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/firebase/firebase.utils.js
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const config = {
   measurementId: 'G-5FR27YDSNP',
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
@@ -54,7 +56,20 @@ export const addCollectionAndDocuments = async (
   return await batch.commit();
 };
 
-firebase.initializeApp(config);
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  console.log(transformedCollection);
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/pages/shop/shop.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/pages/shop/shop.component.jsx
@@ -4,11 +4,34 @@ import { Route } from 'react-router-dom';
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection('collections');
+
+    collectionRef.onSnapshot(async (snapshot) => {
+      convertCollectionsSnapshotToMap(snapshot);
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
