ec657c1040605090ee3e5f65a960eb58ee92a89e
dobernike <Dobernike@mail.ru>
2020-04-17 15:12:34 +0300

    Moving Shop Data to Firebase

--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/App.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/App.jsx
@@ -22,6 +22,7 @@ class App extends React.Component {
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
--- practice/JavaScript/React/zero to mastery/crwn-clothing/src/firebase/firebase.utils.js
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/src/firebase/firebase.utils.js
@@ -1,16 +1,16 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDGUrmwgZZp_P6hg5wotGgOiJ3pIfRutjA",
-  authDomain: "crwn-db-4cb94.firebaseapp.com",
-  databaseURL: "https://crwn-db-4cb94.firebaseio.com",
-  projectId: "crwn-db-4cb94",
-  storageBucket: "crwn-db-4cb94.appspot.com",
-  messagingSenderId: "815923001715",
-  appId: "1:815923001715:web:f6475cb411f30bf0cfad06",
-  measurementId: "G-5FR27YDSNP",
+  apiKey: 'AIzaSyDGUrmwgZZp_P6hg5wotGgOiJ3pIfRutjA',
+  authDomain: 'crwn-db-4cb94.firebaseapp.com',
+  databaseURL: 'https://crwn-db-4cb94.firebaseio.com',
+  projectId: 'crwn-db-4cb94',
+  storageBucket: 'crwn-db-4cb94.appspot.com',
+  messagingSenderId: '815923001715',
+  appId: '1:815923001715:web:f6475cb411f30bf0cfad06',
+  measurementId: 'G-5FR27YDSNP',
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
@@ -32,11 +32,26 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData,
       });
     } catch (error) {
-      console.log("error creating user", error.message);
+      console.log('error creating user', error.message);
     }
   }
 
   return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
 };
 
 firebase.initializeApp(config);
@@ -45,7 +60,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
