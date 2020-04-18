43d300846a430c5e7788ff51f238f0212ab647af
dobernike <Dobernike@mail.ru>
2020-04-18 16:23:02 +0300

    root saga

--- practice/JavaScript/React/zero to mastery/crwn-clothing/saga-way/src/redux/shop/shop.sagas.js
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/saga-way/src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeEvery, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put } from 'redux-saga/effects';
 
 import {
   firestore,
@@ -27,7 +27,7 @@ export function* fetchCollectionsAsync() {
 }
 
 export function* fetchCollectionsStart() {
-  yield takeEvery(
+  yield takeLatest(
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
--- practice/JavaScript/React/zero to mastery/crwn-clothing/saga-way/src/redux/store.js
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/saga-way/src/redux/store.js
@@ -3,9 +3,8 @@ import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
-import { fetchCollectionsStart } from './shop/shop.sagas';
-
 import rootReducer from './root-reducer';
+import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -17,6 +16,6 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
