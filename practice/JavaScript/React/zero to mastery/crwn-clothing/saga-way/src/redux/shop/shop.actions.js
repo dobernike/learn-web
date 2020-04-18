e65ea40c4b835fd734f36285e0e1c51e6358dc60
dobernike <Dobernike@mail.ru>
2020-04-18 16:13:18 +0300

    Redux Thunk into Saga

--- practice/JavaScript/React/zero to mastery/crwn-clothing/saga-way/src/redux/shop/shop.actions.js
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/saga-way/src/redux/shop/shop.actions.js
@@ -1,10 +1,5 @@
 import ShopActionTypes from './shop.types';
 
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from '../../firebase/firebase.utils';
-
 export const fetchCollectionsStart = () => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_START,
 });
@@ -18,18 +13,3 @@ export const fetchCollectionsFailure = (errorMessage) => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
   payload: errorMessage,
 });
-
-export const fetchCollectionsStartAsync = () => {
-  return (dispatch) => {
-    const collectionRef = firestore.collection('collections');
-    dispatch(fetchCollectionsStart());
-
-    collectionRef
-      .get()
-      .then((snapshot) => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        dispatch(fetchCollectionsSuccess(collectionsMap));
-      })
-      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
-  };
-};
--- practice/JavaScript/React/zero to mastery/crwn-clothing/saga-way/src/redux/shop/shop.sagas.js
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/saga-way/src/redux/shop/shop.sagas.js
@@ -1,9 +1,29 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from './shop.actions';
 
 import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
-  yield console.log('I am fired');
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
