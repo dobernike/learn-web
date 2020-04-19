761133873c01a39e3be7ac792786ff6894df09d0
dobernike <Dobernike@mail.ru>
2020-04-19 13:10:03 +0300

    Connecting client to server

--- practice/JavaScript/React/zero to mastery/crwn-clothing/hooks-way/client/src/components/stripe-button/stripe-button.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/hooks-way/client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_yz8mOpvrVug7sSnXTUH4q0EA00VyqfxbRy';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('Payment successful');
+      })
+      .catch((error) => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Please sure you use the provided credit card.'
+        );
+      });
   };
 
   return (
