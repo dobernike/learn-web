8f95a655a30d028218e673c648a1710349a03b63
dobernike <Dobernike@mail.ru>
2020-04-19 11:14:23 +0300

    rename

---
+++ practice/JavaScript/React/zero to mastery/hooks-explained/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import UseReducerExample from './components/use-reducer-example/use-reducer-example.component';
+
+import './App.css';
+
+const App = (props) => {
+  return (
+    <div className="App">
+      <UseReducerExample />
+    </div>
+  );
+};
+
+export default App;
---
+++ practice/JavaScript/React/zero to mastery/hooks-explained/src/components/use-reducer-example/use-reducer-example.component.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+import Card from '../card/card.component';
+
+const UseReducerExample = () => {
+  const [user, setUser] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    if (searchQuery.length > 0) {
+      const fetchFunc = async () => {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users?username=${searchQuery}`
+        );
+        const resJson = await response.json();
+        setUser(resJson[0]);
+      };
+
+      fetchFunc();
+    }
+  }, [searchQuery]);
+
+  return (
+    <Card>
+      <input
+        type="search"
+        value={searchQuery}
+        onChange={(event) => setSearchQuery(event.target.value)}
+      />
+      {user ? (
+        <div>
+          <h3>{user.name}</h3>
+          <h3> {user.username} </h3>
+          <h3> {user.email} </h3>
+        </div>
+      ) : (
+        <p>No user found</p>
+      )}
+    </Card>
+  );
+};
+
+export default UseReducerExample;
