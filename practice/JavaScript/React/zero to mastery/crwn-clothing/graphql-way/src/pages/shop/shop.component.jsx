4c1c918bb37dbbf284f9ba339fdfd215f80776dd
dobernike <Dobernike@mail.ru>
2020-04-20 09:07:38 +0300

    Query with variables

--- practice/JavaScript/React/zero to mastery/crwn-clothing/graphql-way/src/pages/shop/shop.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/graphql-way/src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import { default as CollectionsOverview } from '../../components/collections-overview/collections-overview.container';
-import CollectionPage from '../collection/collection.component';
+import { default as CollectionPage } from '../collection/collection.container';
 
 const ShopPage = ({ match }) => (
   <div className="shop-page">
