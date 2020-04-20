98e9bc8f81e8496bada5ec00842af680595d1625
dobernike <Dobernike@mail.ru>
2020-04-20 08:21:36 +0300

    Apollo container

--- practice/JavaScript/React/zero to mastery/crwn-clothing/graphql-way/src/pages/shop/shop.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/graphql-way/src/pages/shop/shop.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import { default as CollectionsOverview } from '../../components/collections-overview/collections-overview.container';
 import CollectionPage from '../collection/collection.component';
 
 const ShopPage = ({ match }) => (
-  <div className='shop-page'>
+  <div className="shop-page">
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
     <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
