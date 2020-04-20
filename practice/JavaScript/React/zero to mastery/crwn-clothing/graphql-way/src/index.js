137a14e15baddbcd27abcdb7e138957afe7a5e81
dobernike <Dobernike@mail.ru>
2020-04-20 20:29:30 +0300

    redux -> apollo graphql

--- practice/JavaScript/React/zero to mastery/crwn-clothing/graphql-way/src/index.js
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/graphql-way/src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-boost';
 
-import { store, persistor } from './redux/store';
-
 import './index.css';
-import App from './App';
+import { default as App } from './App/App.container';
 import { resolvers, typeDefs } from './graphql/resolvers';
+import { default as data } from './graphql/initial-data';
 
 const httpLink = createHttpLink({
   uri: 'https://crwn-clothing.com',
@@ -27,23 +24,13 @@ const client = new ApolloClient({
   resolvers,
 });
 
-client.writeData({
-  data: {
-    cartHidden: true,
-    cartItems: [],
-    itemCount: 0,
-  },
-});
+client.writeData({ data });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Provider store={store}>
-      <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ApolloProvider>,
   document.getElementById('root')
 );
--- practice/JavaScript/React/zero to mastery/crwn-clothing/graphql-way/src/pages/checkout/checkout.component.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/graphql-way/src/pages/checkout/checkout.component.jsx
@@ -1,41 +1,34 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import { default as CheckoutItem } from '../../components/checkout-item/checkout-item.container';
 import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
-
-import {
-  selectCartItems,
-  selectCartTotal
-} from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
 
 const CheckoutPage = ({ cartItems, total }) => (
-  <div className='checkout-page'>
-    <div className='checkout-header'>
-      <div className='header-block'>
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
         <span>Product</span>
       </div>
-      <div className='header-block'>
+      <div className="header-block">
         <span>Description</span>
       </div>
-      <div className='header-block'>
+      <div className="header-block">
         <span>Quantity</span>
       </div>
-      <div className='header-block'>
+      <div className="header-block">
         <span>Price</span>
       </div>
-      <div className='header-block'>
+      <div className="header-block">
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map(cartItem => (
+    {cartItems.map((cartItem) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className='total'>TOTAL: ${total}</div>
-    <div className='test-warning'>
+    <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
       *Please use the following test credit card for payments*
       <br />
       4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
@@ -44,9 +37,4 @@ const CheckoutPage = ({ cartItems, total }) => (
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems,
-  total: selectCartTotal
-});
-
-export default connect(mapStateToProps)(CheckoutPage);
+export default CheckoutPage;
