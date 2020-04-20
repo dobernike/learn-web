259214c378e4bb01cc58c435fe7f919bbce273ae
dobernike <Dobernike@mail.ru>
2020-04-20 20:59:28 +0300

    Mobile Responsiveness

--- practice/JavaScript/React/zero to mastery/crwn-clothing/hooks-way/client/src/App.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/hooks-way/client/src/App.jsx
@@ -13,7 +13,7 @@ import Header from './components/header/header.component';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
-import './App.css';
+import { GlobalStyle } from './global.styles';
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
@@ -22,6 +22,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
--- practice/JavaScript/React/zero to mastery/crwn-clothing/hooks-way/client/src/components/collection-item/collection-item.styles.jsx
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/hooks-way/client/src/components/collection-item/collection-item.styles.jsx
@@ -14,9 +14,24 @@ export const CollectionItemContainer = styled.div`
     .image {
       opacity: 0.8;
     }
+
     button {
       opacity: 0.85;
       display: flex;
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    width: 40vw;
+
+    &:hover {
+      .image {
+        opacity: unset;
+      }
+
+      button {
+        opacity: unset;
+      }
     }
   }
 `;
@@ -27,6 +42,13 @@ export const AddButton = styled(CustomButton)`
   position: absolute;
   top: 255px;
   display: none;
+
+  @media screen and (max-width: 800px) {
+    display: block;
+    opacity: 0.9;
+    min-width: unset;
+    padding: 0 10px;
+  }
 `;
 
 export const BackgroundImage = styled.div`
