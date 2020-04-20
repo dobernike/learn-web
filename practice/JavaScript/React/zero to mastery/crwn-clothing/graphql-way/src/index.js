873018c9ff24a68bfeeef3fcff3b670bd7a79028
dobernike <Dobernike@mail.ru>
2020-04-20 08:10:50 +0300

    add apollo

--- practice/JavaScript/React/zero to mastery/crwn-clothing/graphql-way/src/index.js
+++ practice/JavaScript/React/zero to mastery/crwn-clothing/graphql-way/src/index.js
@@ -3,19 +3,55 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, gql } from 'apollo-boost';
 
 import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
 
+const httpLink = createHttpLink({
+  uri: 'https://crwn-clothing.com',
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+client
+  .query({
+    query: gql`
+      {
+        getCollectionsByTitle(title: "hats") {
+          id
+          title
+          items {
+            id
+            name
+            price
+            imageUrl
+          }
+        }
+      }
+    `,
+  })
+  .then((res) => console.log(res));
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
