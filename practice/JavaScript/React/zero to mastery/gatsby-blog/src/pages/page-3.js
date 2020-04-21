44dfefbbef1eacbd7b9f90995c83524bce4f9e0a
dobernike <Dobernike@mail.ru>
2020-04-21 12:57:26 +0300

    gatsby pages

---
+++ practice/JavaScript/React/zero to mastery/gatsby-blog/src/pages/page-3.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>HELLO FROM THE THIRD PAGE</h1>
+  </Layout>
+)
+
+export default ThirdPage
