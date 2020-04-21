1b039e3b7decc082c5b521bd679b00abb7aa6675
dobernike <Dobernike@mail.ru>
2020-04-21 15:07:05 +0300

    Add post word

--- practice/JavaScript/React/zero to mastery/gatsby-blog/src/pages/index.js
+++ practice/JavaScript/React/zero to mastery/gatsby-blog/src/pages/index.js
@@ -20,7 +20,7 @@ export default ({ data }) => (
     <SEO title="Home" />
     <div>
       <h1>Dobernike`s Thoughts</h1>
-      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
           <BlogLink to={node.fields.slug}>
