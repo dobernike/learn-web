8c4aa347f870e9dda5b4d45884cad7494a8674d1
dobernike <Dobernike@mail.ru>
2020-04-22 20:30:54 +0300

    error boundary

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/React-Redux -Module-Bundling/robofriends/src/containers/App.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/React-Redux -Module-Bundling/robofriends/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 class App extends Component {
@@ -36,7 +37,9 @@ class App extends Component {
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
         </Scroll>
       </div>
     );
