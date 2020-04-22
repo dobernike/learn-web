050277cb9eb84b88d5be2ac0872d623d0af2255d
dobernike <Dobernike@mail.ru>
2020-04-22 19:50:49 +0300

    6

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/React-Redux -Module-Bundling/robofriends/src/containers/App.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/React-Redux -Module-Bundling/robofriends/src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -24,17 +24,14 @@ class App extends Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter((robots) => {
-      return robots.name
-        .toLocaleLowerCase()
-        .includes(this.state.searchfield.toLocaleLowerCase());
-    });
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) =>
+      robot.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase())
+    );
 
-    if (!this.state.robots.length) {
-      return <h1>Loading</h1>;
-    }
-
-    return (
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
