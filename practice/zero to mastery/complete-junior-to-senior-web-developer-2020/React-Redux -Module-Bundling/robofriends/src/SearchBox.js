93d3e093fd674fcf4e3ab3a83fde705fe6559852
dobernike <Dobernike@mail.ru>
2020-04-22 18:30:32 +0300

    4

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/React-Redux -Module-Bundling/robofriends/src/SearchBox.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/React-Redux -Module-Bundling/robofriends/src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => {
   return (
     <div className="pa2">
       <input
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="search robots"
+        onChange={searchChange}
       />
     </div>
   );
