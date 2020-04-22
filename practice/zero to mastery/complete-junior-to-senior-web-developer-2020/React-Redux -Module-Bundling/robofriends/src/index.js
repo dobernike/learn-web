8946b9f84da5506693f0ae23a4d25be32d80c311
dobernike <Dobernike@mail.ru>
2020-04-22 17:30:41 +0300

    1

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/React-Redux -Module-Bundling/robofriends/src/index.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/React-Redux -Module-Bundling/robofriends/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Card />
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
   </React.StrictMode>,
   document.getElementById('root')
 );
