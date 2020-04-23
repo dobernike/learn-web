85ff5fe35cf7909576641edb577bf7d2835f0f19
dobernike <Dobernike@mail.ru>
2020-04-23 08:24:11 +0300

    fix lazy

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/others/code-splitting-exercise/classic/src/App.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/others/code-splitting-exercise/classic/src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 import Page1 from './Components/Page1';
 // Part 1 - No Code Splitting
-import Page2 from './Components/Page2';
-import Page3 from './Components/Page3';
+// import Page2 from './Components/Page2';
+// import Page3 from './Components/Page3';
 // Part 3 - Cleaner Code Splitting
-// import AsyncComponent from './AsyncComponent';
+import AsyncComponent from './AsyncComponent';
 
 class App extends Component {
   constructor() {
@@ -15,7 +15,7 @@ class App extends Component {
       route: 'page1',
       // Part 2 - Code Splitting - manual
       // component: null
-    }
+    };
   }
   onRouteChange = (route) => {
     // Part 1 - No Code Splitting
@@ -39,16 +39,16 @@ class App extends Component {
     //       console.log(err)
     //     });
     // }
-  }
+  };
   render() {
     // Part 1 - No code splitting
-    if (this.state.route === 'page1') {
-      return <Page1 onRouteChange={this.onRouteChange} />
-    } else if (this.state.route === 'page2') {
-      return <Page2 onRouteChange={this.onRouteChange} />
-    } else {
-      return <Page3 onRouteChange={this.onRouteChange} />
-    }
+    // if (this.state.route === 'page1') {
+    //   return <Page1 onRouteChange={this.onRouteChange} />
+    // } else if (this.state.route === 'page2') {
+    //   return <Page2 onRouteChange={this.onRouteChange} />
+    // } else {
+    //   return <Page3 onRouteChange={this.onRouteChange} />
+    // }
 
     // Part 2 - No Code Splitting - manual
     // if (this.state.route === 'page1') {
@@ -58,16 +58,15 @@ class App extends Component {
     // }
 
     // Part 3 - Cleaner Code Splitting
-    // if (this.state.route === 'page1') {
-    //   return <Page1 onRouteChange={this.onRouteChange} />
-    // } else if (this.state.route === 'page2') {
-    //   const AsyncPage2 = AsyncComponent(() => import("./Components/Page2"));
-    //   return <AsyncPage2 onRouteChange={this.onRouteChange} />
-    // } else {
-    //   const AsyncPage3 = AsyncComponent(() => import("./Components/Page3"));
-    //   return <AsyncPage3 onRouteChange={this.onRouteChange} />
-    // }
-
+    if (this.state.route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange} />;
+    } else if (this.state.route === 'page2') {
+      const AsyncPage2 = AsyncComponent(() => import('./Components/Page2'));
+      return <AsyncPage2 onRouteChange={this.onRouteChange} />;
+    } else {
+      const AsyncPage3 = AsyncComponent(() => import('./Components/Page3'));
+      return <AsyncPage3 onRouteChange={this.onRouteChange} />;
+    }
   }
 }
 
