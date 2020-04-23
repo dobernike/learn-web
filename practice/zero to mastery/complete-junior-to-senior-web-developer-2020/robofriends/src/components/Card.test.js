b4c0c1d89a11fe67efc72d2cc07a152e70511dc1
dobernike <Dobernike@mail.ru>
2020-04-23 11:30:49 +0300

    Snapshot testing

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/robofriends/src/components/Card.test.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/robofriends/src/components/Card.test.js
@@ -1,7 +1,7 @@
+import { shallow } from 'enzyme';
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
 import Card from './Card';
 
 it('expect to render Card component', () => {
-  expect(shallow(<Card />).length).toEqual(1);
+  expect(shallow(<Card />)).toMatchSnapshot();
 });
