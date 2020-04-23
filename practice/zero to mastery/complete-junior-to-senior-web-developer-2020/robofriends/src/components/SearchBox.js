10c0dc32bdeab47301d631b00f701e2a7647b135
dobernike <Dobernike@mail.ru>
2020-04-23 10:01:52 +0300

    PWA Solution 1

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/robofriends/src/components/SearchBox.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/robofriends/src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ searchChange }) => {
   return (
     <div className="pa2">
       <input
+        aria-label="Search Robot"
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="search robots"
