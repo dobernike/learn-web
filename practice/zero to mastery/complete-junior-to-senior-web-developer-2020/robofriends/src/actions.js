0febdb293a6287210075f8c0c16518bf140b2423
dobernike <Dobernike@mail.ru>
2020-04-23 19:49:31 +0300

    Testing actions

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/robofriends/src/actions.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/robofriends/src/actions.js
@@ -1,3 +1,4 @@
+import { apiCall } from './api/api';
 import {
   CHANGE_SEARCH_FIELD,
   REQUEST_ROBOTS_PENDING,
@@ -12,8 +13,7 @@ export const setSearchField = (text) => ({
 
 export const requestRobots = () => (dispatch) => {
   dispatch({ type: REQUEST_ROBOTS_PENDING });
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
+  apiCall('https://jsonplaceholder.typicode.com/users')
     .then((data) => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
     .catch((error) =>
       dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error })
---
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/robofriends/src/actions.test.js
@@ -0,0 +1,32 @@
+import * as actions from './actions';
+
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+} from './constants';
+
+import configureMockStore from 'redux-mock-store';
+import thunkMiddleware from 'redux-thunk';
+
+const mockStore = configureMockStore([thunkMiddleware]);
+
+it('should create an action to search robots', () => {
+  const text = 'wooo';
+  const expectedAction = {
+    type: CHANGE_SEARCH_FIELD,
+    payload: text,
+  };
+  expect(actions.setSearchField(text)).toEqual(expectedAction);
+});
+
+it('handles requesting robots API pending', () => {
+  const store = mockStore();
+  store.dispatch(actions.requestRobots());
+  const action = store.getActions();
+  const expectedAction = {
+    type: REQUEST_ROBOTS_PENDING,
+  };
+  expect(action[0]).toEqual(expectedAction);
+});
