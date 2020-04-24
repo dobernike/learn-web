8efea33ec3def8f45434f440dda92dbce487e0d8
dobernike <Dobernike@mail.ru>
2020-04-24 09:52:10 +0300

    Dynamic apps with Next.js

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/next-ssr/pages/index.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/next-ssr/pages/index.js
@@ -4,8 +4,10 @@ const Index = () => (
   <div>
     <h1>SSR Magician</h1>
     <Link href="/about">
-      {/* <a>About</a> */}
       <button>About</button>
+    </Link>
+    <Link href="/robots">
+      <button>Robots</button>
     </Link>
   </div>
 );
