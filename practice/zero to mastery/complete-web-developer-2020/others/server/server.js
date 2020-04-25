2ee94175749ab43ae964c2d84dd4da5e81e13c4d
dobernike <Dobernike@mail.ru>
2020-04-25 19:56:53 +0300

    RESTful APIs

--- practice/zero to mastery/complete-web-developer-2020/others/server/server.js
+++ practice/zero to mastery/complete-web-developer-2020/others/server/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('<h1>Hello</h1>');
-  next();
-});
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('testtest');
-});
+// app.get('/:id', (req, res) => {
+//   // console.log(req.query);
+//   // console.log(req.body)
+//   // console.log(req.headers);
+//   console.log(req.params);
+//   res.status(404).send('not found');
+//   // res.send('getting root');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
