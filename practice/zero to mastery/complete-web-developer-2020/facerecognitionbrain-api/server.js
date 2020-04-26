898938de01b5395d874e4bae8de265add58b3dcf
dobernike <Dobernike@mail.ru>
2020-04-26 11:45:25 +0300

    setup DB

--- practice/zero to mastery/complete-web-developer-2020/facerecognitionbrain-api/server.js
+++ practice/zero to mastery/complete-web-developer-2020/facerecognitionbrain-api/server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs'); // depricated use bcrypt or bcrypt.js
 const cors = require('cors');
+const knex = require('knex');
+
+const postgres = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: '1992',
+    database: 'smart-brain',
+  },
+});
+
+console.log(postgres.select('*').from('users'));
 
 const app = express();
 
