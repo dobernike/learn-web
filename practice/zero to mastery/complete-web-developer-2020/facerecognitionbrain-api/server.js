2f5a627c5e4ce35ce8928b7784c5bcc7b243e8f4
dobernike <Dobernike@mail.ru>
2020-04-26 09:24:17 +0300

    registering users

--- practice/zero to mastery/complete-web-developer-2020/facerecognitionbrain-api/server.js
+++ practice/zero to mastery/complete-web-developer-2020/facerecognitionbrain-api/server.js
@@ -58,7 +58,6 @@ app.post('/register', (req, res) => {
     id: '125',
     name: name,
     email: email,
-    password: password,
     entries: 0,
     joined: new Date(),
   });
--- practice/zero to mastery/complete-web-developer-2020/facerecognitionbrain/src/App.js
+++ practice/zero to mastery/complete-web-developer-2020/facerecognitionbrain/src/App.js
@@ -35,8 +35,27 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: '',
+      },
     };
   }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
   calculateFaceLocation = (data) => {
     const clarifaiFace =
@@ -101,7 +120,10 @@ class App extends Component {
         ) : route === 'signin' ? (
           <Signin onRouteChange={this.onRouteChange} />
         ) : (
-          <Register onRouteChange={this.onRouteChange} />
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
         )}
       </div>
     );
