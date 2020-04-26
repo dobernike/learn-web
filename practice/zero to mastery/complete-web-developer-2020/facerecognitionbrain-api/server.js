8ec73cfc04e1f899f595c53ef8a2da6649040ac3
dobernike <Dobernike@mail.ru>
2020-04-26 08:33:13 +0300

    profileid and image

--- practice/zero to mastery/complete-web-developer-2020/facerecognitionbrain-api/server.js
+++ practice/zero to mastery/complete-web-developer-2020/facerecognitionbrain-api/server.js
@@ -54,6 +54,35 @@ app.post('/register', (req, res) => {
   res.json(database.users[database.users.length - 1]);
 });
 
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(404).json('user not found');
+  }
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(404).json('user not found');
+  }
+});
+
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 });
