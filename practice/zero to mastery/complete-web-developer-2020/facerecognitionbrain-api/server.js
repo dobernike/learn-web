65e742488b2c58517d7653ad40e712b35c5de36c
dobernike <Dobernike@mail.ru>
2020-04-26 08:50:11 +0300

    storing user passwords

--- practice/zero to mastery/complete-web-developer-2020/facerecognitionbrain-api/server.js
+++ practice/zero to mastery/complete-web-developer-2020/facerecognitionbrain-api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs'); // depricated use bcrypt or bcrypt.js
 
 const app = express();
 
@@ -11,7 +12,6 @@ const database = {
       id: '123',
       name: 'John',
       email: 'john@gmail.com',
-      password: 'cookies',
       entries: 0,
       joined: new Date(),
     },
@@ -19,9 +19,15 @@ const database = {
       id: '124',
       name: 'Sally',
       email: 'sally@gmail.com',
-      password: 'bananas',
       entries: 0,
       joined: new Date(),
+    },
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'john@gmail.com',
     },
   ],
 };
@@ -31,6 +37,16 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
+  const hash = '$2a$10$G5q3FeDAdydYf2rmUrF5DOFgLrKVmEYoUKaYQzKGtYTj/MY4hsP5C';
+  bcrypt.compare('apples', hash, function (err, res) {
+    // res == true
+    console.log('first', res);
+  });
+
+  bcrypt.compare('asf', hash, function (err, res) {
+    // res == true
+    console.log('second', res);
+  });
   if (
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
@@ -43,6 +59,11 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
+  //   bcrypt.hash(password, null, null, function (err, hash) {
+  //     // Store hash in your password DB.
+  //     console.log(hash);
+  //   });
+
   database.users.push({
     id: '125',
     name: name,
