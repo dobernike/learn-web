0fabbeac4dc6a005de490a9686bbea552ded4a73
dobernike <Dobernike@mail.ru>
2020-04-26 20:22:11 +0300

    Dockerfile

---
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain-api/Dockerfile
@@ -0,0 +1,9 @@
+FROM node:erbium
+
+WORKDIR /usr/src/smart-brain-api
+
+COPY ./ ./
+
+RUN npm install
+
+CMD ["/bin/bash"]
--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain-api/server.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain-api/server.js
@@ -25,7 +25,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send(db.users);
+  res.send('ITS WORKING');
 });
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', (req, res) => {
