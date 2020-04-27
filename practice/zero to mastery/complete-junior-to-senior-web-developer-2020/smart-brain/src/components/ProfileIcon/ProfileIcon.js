e6e8f53f2a7ff16e18126fa533daa5de5133a756
dobernike <dobernike@mail.ru>
2020-04-27 20:46:55 +0300

    profile dropdown

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain/src/components/ProfileIcon/ProfileIcon.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain/src/components/ProfileIcon/ProfileIcon.js
@@ -1,20 +1,52 @@
 import React from 'react';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
 
 class ProfileIcon extends React.Component {
   constructor(props) {
     super(props);
+    this.toggle = this.toggle.bind(this);
     this.state = {
       dropdownOpen: false,
     };
   }
+
+  toggle() {
+    this.setState((prevState) => ({
+      dropdownOpen: !prevState.dropdownOpen,
+    }));
+  }
+
   render() {
     return (
       <div className="pa4 tc">
-        <img
-          src="http://tachyons.io/img/logo.jpg"
-          className="br-100 ba h3 w3 dib"
-          alt="avatar"
-        />
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle
+            tag="span"
+            data-toggle="dropdown"
+            aria-expanded={this.state.dropdownOpen}
+          >
+            <img
+              src="http://tachyons.io/img/logo.jpg"
+              className="br-100 ba h3 w3 dib"
+              alt="avatar"
+            />
+          </DropdownToggle>
+          <DropdownMenu
+            className="b--transparent shadow-5"
+            style={{
+              marginTop: '20px',
+              backgroundColor: 'rgba(255, 255, 255, 0.5)',
+            }}
+          >
+            <DropdownItem>View Profile</DropdownItem>
+            <DropdownItem>Signout</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </div>
     );
   }
