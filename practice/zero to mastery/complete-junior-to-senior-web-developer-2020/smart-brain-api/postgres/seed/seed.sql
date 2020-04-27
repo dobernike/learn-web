8fd2577b6f36c31a85c5ea6c67f313ff073dbeed
dobernike <dobernike@mail.ru>
2020-04-27 20:03:20 +0300

    update seed

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain-api/postgres/seed/seed.sql
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain-api/postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (name, email, entries, joined) VALUES ('Jessie', 'jessie@gmail.com', 5, '2018-01-01');
-INSERT INTO login (hash, email) VALUES ('HashHashHash124214ewfe', 'jessie@gmail.com');
+INSERT INTO users (name, email, entries, joined) VALUES ('a', 'a@a.com', 5, '2018-01-01');
+INSERT INTO login (hash, email) VALUES ('$2a$10$B0l88Dey1mQlZnFmnPAppOmUni..g3B9Tmb6ms/My3w7dof2LrEna', 'a@a.com');
 
 COMMIT;
