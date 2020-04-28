e1c36f18d918fd452693ae7df24d4497f2fbcaeb
dobernike <dobernike@mail.ru>
2020-04-28 22:33:36 +0300

    Storing JWT token

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain-api/controllers/signin.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain-api/controllers/signin.js
@@ -40,11 +40,15 @@ const signToken = (email) => {
   return jwt.sign(jwtPayload, 'JWT_SECRET', { expiresIn: '2 days' });
 };
 
+const setToken = (key, value) => {
+ return Promise.resolve(redisClient.set(key, value))
+}
+
 const createSessions = (user) => {
   // JWT token, return user data
   const { email, id } = user;
   const token = signToken(email);
-  return { success: 'true', userId: id, token };
+  return setToken(token, id).then(() => ({ success: 'true', userId: id, token })).catch(console.log)
 };
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
