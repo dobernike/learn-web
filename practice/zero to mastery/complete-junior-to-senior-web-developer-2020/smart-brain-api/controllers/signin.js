3198a1ebda5d4d9d80cf74be221a81c07853d2e8
dobernike <dobernike@mail.ru>
2020-04-28 22:43:52 +0300

    retrieving auth token

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain-api/controllers/signin.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain-api/controllers/signin.js
@@ -30,8 +30,14 @@ const handleSignin = (db, bcrypt, req, res) => {
     .catch((err) => Promise.reject('wrong credentials'));
 };
 
-const getAuthTokenId = () => {
-  console.log('auth ok');
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers;
+  return redisClient.get(authorization, (err, reply) => {
+    if (err || !reply) {
+      return res.status(400).json('Unauthorized')
+    }
+    return res.json({id: reply});
+  });
 };
 
 const signToken = (email) => {
@@ -54,7 +60,7 @@ const createSessions = (user) => {
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers;
   return authorization
-    ? getAuthTokenId()
+    ? getAuthTokenId(req, res)
     : handleSignin(db, bcrypt, req, res)
         .then((data) => {
           return data.id && data.email
