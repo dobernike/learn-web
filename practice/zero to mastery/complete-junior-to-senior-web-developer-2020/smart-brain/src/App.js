c854a01cfd76431cdb6d530b260d9ed2b2bcc280
dobernike <dobernike@mail.ru>
2020-04-29 08:04:04 +0300

    Session sign in

--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain/src/App.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain/src/App.js
@@ -61,7 +61,21 @@ class App extends Component {
         .then((resp) => resp.json())
         .then((data) => {
           if (data && data.id) {
-            console.log("success we need to get user profile");
+            fetch(`http://localhost:3000/profile/${data.id}`, {
+              method: "get",
+              headers: {
+                "Content-Type": "application/json",
+                // Authorization: "Bearer " + token,
+                Authorization: token,
+              },
+            })
+              .then((resp) => resp.json())
+              .then((user) => {
+                if (user && user.email) {
+                  this.loadUser(user);
+                  this.onRouteChange("home");
+                }
+              });
           }
         })
         .catch(console.log);
--- practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain/src/components/Signin/Signin.js
+++ practice/zero to mastery/complete-junior-to-senior-web-developer-2020/smart-brain/src/components/Signin/Signin.js
@@ -35,8 +35,22 @@ class Signin extends React.Component {
       .then((data) => {
         if (data.userId && data.success === "true") {
           this.saveAuthTokenInSession(data.token);
-          this.props.loadUser(data);
-          this.props.onRouteChange("home");
+          fetch(`http://localhost:3000/profile/${data.userId}`, {
+            method: "get",
+            headers: {
+              "Content-Type": "application/json",
+              // Authorization: "Bearer " + token,
+              Authorization: data.token,
+            },
+          })
+            .then((resp) => resp.json())
+            .then((user) => {
+              if (user && user.email) {
+                this.props.loadUser(user);
+                this.props.onRouteChange("home");
+              }
+            })
+            .catch(console.log);
         }
       });
   };
