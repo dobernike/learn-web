9130ab718d1d8c1dec6c2b70e5c677ea41bc80f8
dobernike <Dobernike@mail.ru>
2020-05-04 20:48:00 +0300

    spread operator (...)

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/next-gen-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/next-gen-01-starting-setup/src/app.ts
@@ -23,3 +23,15 @@ if (button) {
 }
 
 printOutput(add(5));
+
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  name: 'Max',
+  age: 30,
+};
+
+const copiedPerson = { ...person };
