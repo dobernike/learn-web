fa792541f496c91ad8b518178838cce8624e95bd
dobernike <Dobernike@mail.ru>
2020-05-04 20:22:21 +0300

    Ts types vs Js types

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/course-starting-project/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/course-starting-project/app.ts
@@ -1,4 +1,7 @@
 function add(n1: number, n2: number) {
+  //   if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //     throw new Error('Incorrect input!');
+  //   }
   return n1 + n2;
 }
 
