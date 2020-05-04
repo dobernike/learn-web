6c0b967167989e28de04a54ccbbd1e3853e083cc
dobernike <Dobernike@mail.ru>
2020-05-04 20:40:46 +0300

    arrow fn

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/next-gen-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/next-gen-01-starting-setup/src/app.ts
@@ -3,8 +3,23 @@ let age = 30;
 
 age = 29;
 
-function add(a: number, b: number) {
-  let result;
-  result = a + b;
-  return result;
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
+// }
+
+const add = (a: number, b: number) => a + b;
+
+console.log(add(2, 5));
+
+const printOutput: (a: number | string) => void = (output) =>
+  console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', (event) => console.log(event));
 }
+
+printOutput(add(5, 2));
