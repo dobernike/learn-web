82cad8d86761d68bbe7c5551faee4c45f7823b78
dobernike <Dobernike@mail.ru>
2020-05-05 22:40:48 +0300

    Overriding Properties & The protected Modifier

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/cl-interfaces-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   //   private readonly id: string;
   //   private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -36,6 +36,13 @@ class AccountingDepartment extends Department {
     super(id, 'Accounting');
   }
 
+  addEmployee(name: string) {
+    if (name === 'Max') {
+      return;
+    }
+    this.employees.push(name);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -62,7 +69,11 @@ const accounting = new AccountingDepartment('d2', []);
 
 accounting.addReport('Something went wrong...');
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
 accounting.printReports();
+accounting.printEmployeeInformation();
 
 // const accountingCopy = { name: 's', describe: accounting.describe };
 
