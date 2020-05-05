9c9236927078512c4f34de5628912ad1a672246f
dobernike <Dobernike@mail.ru>
2020-05-05 14:39:53 +0300

    Added practice WebRTC simple Peer

--- practice/WebRTC/chat_simple_peer/client/src/App.js
+++ practice/WebRTC/chat_simple_peer/client/src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
-import io from "socket.io-client";
-import Peer from "simple-peer";
-import styled from "styled-components";
+import io from 'socket.io-client';
+import Peer from 'simple-peer';
+import styled from 'styled-components';
 
 const Container = styled.div`
   height: 100vh;
@@ -23,11 +23,11 @@ const Video = styled.video`
 `;
 
 function App() {
-  const [yourID, setYourID] = useState("");
+  const [yourID, setYourID] = useState('');
   const [users, setUsers] = useState({});
   const [stream, setStream] = useState();
   const [receivingCall, setReceivingCall] = useState(false);
-  const [caller, setCaller] = useState("");
+  const [caller, setCaller] = useState('');
   const [callerSignal, setCallerSignal] = useState();
   const [callAccepted, setCallAccepted] = useState(false);
 
@@ -36,46 +36,83 @@ function App() {
   const socket = useRef();
 
   useEffect(() => {
-    socket.current = io.connect("/");
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
-      setStream(stream);
-      if (userVideo.current) {
-        userVideo.current.srcObject = stream;
-      }
-    })
+    socket.current = io.connect('/');
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        setStream(stream);
+        if (userVideo.current) {
+          userVideo.current.srcObject = stream;
+        }
+      });
 
-    socket.current.on("yourID", (id) => {
+    socket.current.on('yourID', (id) => {
       setYourID(id);
-    })
-    socket.current.on("allUsers", (users) => {
+    });
+    socket.current.on('allUsers', (users) => {
       setUsers(users);
-    })
+    });
 
-    socket.current.on("hey", (data) => {
-     
-    })
+    socket.current.on('hey', (data) => {
+      setReceivingCall(true);
+      setCaller(data.from);
+      setCallerSignal(data.signal);
+    });
   }, []);
 
   function callPeer(id) {
-    
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream: stream,
+    });
+
+    peer.on('signal', (data) => {
+      socket.current.emit('callUser', {
+        userToCall: id,
+        signalData: data,
+        from: yourID,
+      });
+
+      peer.on('stream', (stream) => {
+        if (partnerVideo.current) {
+          partnerVideo.current.srcObject = stream;
+        }
+      });
+
+      socket.current.on('callAccepted', (signal) => {
+        setCallAccepted(true);
+        peer.signal(signal);
+      });
+    });
   }
 
   function acceptCall() {
-    
+    setCallAccepted(true);
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: stream,
+    });
+    peer.on('signal', (data) => {
+      socket.current.emit('acceptCall', { signal: data, to: caller });
+    });
+
+    peer.on('stream', (stream) => {
+      partnerVideo.current.srcObject = stream;
+    });
+
+    peer.signal(callerSignal);
   }
 
   let UserVideo;
   if (stream) {
-    UserVideo = (
-      <Video playsInline muted ref={userVideo} autoPlay />
-    );
+    UserVideo = <Video playsInline muted ref={userVideo} autoPlay />;
   }
 
   let PartnerVideo;
   if (callAccepted) {
-    PartnerVideo = (
-      <Video playsInline ref={partnerVideo} autoPlay />
-    );
+    PartnerVideo = <Video playsInline ref={partnerVideo} autoPlay />;
   }
 
   let incomingCall;
@@ -85,7 +122,7 @@ function App() {
         <h1>{caller} is calling you</h1>
         <button onClick={acceptCall}>Accept</button>
       </div>
-    )
+    );
   }
   return (
     <Container>
@@ -94,18 +131,14 @@ function App() {
         {PartnerVideo}
       </Row>
       <Row>
-        {Object.keys(users).map(key => {
+        {Object.keys(users).map((key) => {
           if (key === yourID) {
             return null;
           }
-          return (
-            <button onClick={() => callPeer(key)}>Call {key}</button>
-          );
+          return <button onClick={() => callPeer(key)}>Call {key}</button>;
         })}
       </Row>
-      <Row>
-        {incomingCall}
-      </Row>
+      <Row>{incomingCall}</Row>
     </Container>
   );
 }
