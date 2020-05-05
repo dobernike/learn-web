13bd402b7b65b7dc72186a9398bbc123122c6504
dobernike <Dobernike@mail.ru>
2020-05-05 22:53:54 +0300

    Static Methods & Properties

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/cl-interfaces-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,4 +1,5 @@
 class Department {
+  static fiscalYear = 2020;
   //   private readonly id: string;
   //   private name: string;
   protected employees: string[] = [];
@@ -6,6 +7,11 @@ class Department {
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
+    // console.log(Department.fiscalYear);
+  }
+
+  static createEmployee(name: string) {
+    return { name: name };
   }
 
   describe(this: Department) {
@@ -69,6 +75,9 @@ class AccountingDepartment extends Department {
     console.log(this.reports);
   }
 }
+
+const employee1 = Department.createEmployee('Max');
+console.log(employee1, Department.fiscalYear);
 
 const it = new ITDepartment('d1', ['Max']);
 
