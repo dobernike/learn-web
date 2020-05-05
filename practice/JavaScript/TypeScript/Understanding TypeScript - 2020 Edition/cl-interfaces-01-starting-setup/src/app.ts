5813c6bb90c7bf69ae82aa44072bdc1c23935090
dobernike <Dobernike@mail.ru>
2020-05-05 08:12:52 +0300

    constuctor fn and this

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/cl-interfaces-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/cl-interfaces-01-starting-setup/src/app.ts
@@ -4,8 +4,16 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
+
+  describe(this: Department) {
+    console.log(`Department: ${this.name}`);
+  }
 }
 
 const accounting = new Department('Accounting');
 
-console.log(accounting);
+accounting.describe(); // Accounting
+
+const accountingCopy = { name: 's', describe: accounting.describe };
+
+accountingCopy.describe(); // undefined
