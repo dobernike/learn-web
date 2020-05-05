5a020f5526756aa62444f1e73f62518ab5e0fe5e
dobernike <Dobernike@mail.ru>
2020-05-05 08:25:10 +0300

    public and private

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/cl-interfaces-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  name: string; // = public name: string
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,12 +9,28 @@ class Department {
   describe(this: Department) {
     console.log(`Department: ${this.name}`);
   }
+
+  addEmployee(employee: string) {
+    //   validation
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+accounting.employees[2] = 'Anna';
+
 accounting.describe(); // Accounting
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: 's', describe: accounting.describe };
+// const accountingCopy = { name: 's', describe: accounting.describe };
 
-accountingCopy.describe(); // undefined
+// accountingCopy.describe(); // undefined
