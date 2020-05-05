2616ce8e1b269bddbd06ecec4c2b9917dcab3bd9
dobernike <Dobernike@mail.ru>
2020-05-05 22:48:11 +0300

    Getters & Setters

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/cl-interfaces-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/cl-interfaces-01-starting-setup/src/app.ts
@@ -32,8 +32,25 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No report found.');
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error('Please pass in a valid value.');
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, 'Accounting');
+    this.lastReport = reports[0];
   }
 
   addEmployee(name: string) {
@@ -45,6 +62,7 @@ class AccountingDepartment extends Department {
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
@@ -67,7 +85,9 @@ console.log(it);
 
 const accounting = new AccountingDepartment('d2', []);
 
+accounting.mostRecentReport = 'Year End Report';
 accounting.addReport('Something went wrong...');
+console.log(accounting.mostRecentReport);
 
 accounting.addEmployee('Max');
 accounting.addEmployee('Manu');
