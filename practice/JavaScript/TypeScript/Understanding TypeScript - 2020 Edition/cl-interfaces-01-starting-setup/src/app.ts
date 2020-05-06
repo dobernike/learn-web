b3fd79ddb7a109e62039940a7ce6072caddff03e
dobernike <Dobernike@mail.ru>
2020-05-06 22:35:13 +0300

    Interfaces as Function Types

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/cl-interfaces-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,3 +1,14 @@
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
 interface Named {
   readonly name: string;
 }
