9e957bf82817f525439bdd8365bf44b648d883cc
dobernike <Dobernike@mail.ru>
2020-05-07 08:29:59 +0300

    Intersection Types

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/adv-types-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/adv-types-01-starting-setup/src/app.ts
@@ -1,1 +1,34 @@
-// Code goes here!
+// interface Admin {
+//   name: string;
+//   privileges: string[];
+// };
+
+// interface Employee {
+//   name: string;
+//   startDate: Date;
+// };
+
+// interface ElevatedEmployee extends Employee, Admin {}
+
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: 'Max',
+  privileges: ['create-server'],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
