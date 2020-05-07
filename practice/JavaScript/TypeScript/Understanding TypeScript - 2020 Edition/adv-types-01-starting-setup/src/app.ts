602abcc5a18b4cc63f15993f8179231dacbe45d0
dobernike <Dobernike@mail.ru>
2020-05-07 20:27:27 +0300

    Index Properties

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/adv-types-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/adv-types-01-starting-setup/src/app.ts
@@ -122,3 +122,13 @@ const userInputElement = document.getElementById('user-input');
 if (userInputElement) {
   (userInputElement as HTMLInputElement).value = 'Hi there!';
 }
+
+interface ErrorContainer {
+  // { email: 'Not a valid email!', username: 'Must start with a character!' }
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email!',
+  username: 'Must start with a character!',
+};
