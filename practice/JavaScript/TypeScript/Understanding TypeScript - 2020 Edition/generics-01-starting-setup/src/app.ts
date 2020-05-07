f026b73ff8c3085ab6988ef189538465fe3ab247
dobernike <Dobernike@mail.ru>
2020-05-07 22:31:52 +0300

    Built-in Generics

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/generics-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/generics-01-starting-setup/src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+// const names: Array<string> = []; // string[]
+// // names[0].split(' ');
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+
+// promise.then(data => {
+//     // data.split(' ');
+// })
