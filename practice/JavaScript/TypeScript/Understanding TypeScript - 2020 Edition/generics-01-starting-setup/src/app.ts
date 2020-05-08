aaf61d08dd8faa9633e9bf6230b8d6feae8221a5
dobernike <Dobernike@mail.ru>
2020-05-08 07:44:13 +0300

    Another Generic Function

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/generics-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/generics-01-starting-setup/src/app.ts
@@ -17,3 +17,19 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 console.log(mergeObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.';
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(['Sports', 'Coocking']));
