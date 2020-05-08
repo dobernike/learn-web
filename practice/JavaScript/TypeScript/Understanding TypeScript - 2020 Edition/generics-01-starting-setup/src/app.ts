6064be880f01e581779186b393b495cfcf8c68e8
dobernike <Dobernike@mail.ru>
2020-05-08 07:59:09 +0300

    Generic Classes

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/generics-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/generics-01-starting-setup/src/app.ts
@@ -42,3 +42,38 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 extractAndConvert({ name: 'Max' }, 'name');
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1); // -1 for object
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// const maxObj = { name: 'Max' };
+// objStorage.addItem(maxObj);
+// objStorage.addItem({ name: 'Manu' });
+// // ...
+// objStorage.removeItem(maxObj);
+// console.log(objStorage.getItems());
