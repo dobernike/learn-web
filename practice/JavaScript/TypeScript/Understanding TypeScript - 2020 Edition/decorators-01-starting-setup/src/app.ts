79ace019fc1831ed454a2bca406f0f35e01fd0d7
dobernike <Dobernike@mail.ru>
2020-05-08 08:56:59 +0300

    Diving into Property Decorators

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/decorators-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/decorators-01-starting-setup/src/app.ts
@@ -32,3 +32,33 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+// ---
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property decorator!');
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('Invalid price - should be positive!');
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
