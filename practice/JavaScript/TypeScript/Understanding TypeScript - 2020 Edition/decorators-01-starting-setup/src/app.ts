69314807857bd3dfd005799f36d57dd6d6930286
dobernike <Dobernike@mail.ru>
2020-05-08 08:47:42 +0300

    Building More Useful Decorators

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/decorators-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/decorators-01-starting-setup/src/app.ts
@@ -5,7 +5,19 @@ function Logger(logString: string) {
   };
 }
 
-@Logger('LOGGING - PERSON')
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger('LOGGING - PERSON')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
   name = 'Max';
 
