ed67c5b5300048bbc2e03b8a6195e7c8ced24c3a
dobernike <Dobernike@mail.ru>
2020-05-08 08:41:11 +0300

    Working with Decorator Factories

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/decorators-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/decorators-01-starting-setup/src/app.ts
@@ -1,9 +1,11 @@
-function Logger(constructor: Function) {
-  console.log('Logging...');
-  console.log(constructor);
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+@Logger('LOGGING - PERSON')
 class Person {
   name = 'Max';
 
