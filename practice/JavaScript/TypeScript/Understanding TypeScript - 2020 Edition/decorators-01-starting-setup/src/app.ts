b9738c94e13629aa2031cfb58dc49747c677e88f
dobernike <Dobernike@mail.ru>
2020-05-08 08:38:53 +0300

    A First Class Decorator

---
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/decorators-01-starting-setup/src/app.ts
@@ -0,0 +1,17 @@
+function Logger(constructor: Function) {
+  console.log('Logging...');
+  console.log(constructor);
+}
+
+@Logger
+class Person {
+  name = 'Max';
+
+  constructor() {
+    console.log('Creating person object...');
+  }
+}
+
+const pers = new Person();
+
+console.log(pers);
