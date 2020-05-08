1ebbb8babb11a43928d9fbd468ed5eb19b564956
dobernike <Dobernike@mail.ru>
2020-05-08 07:33:46 +0300

    Generic function

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/generics-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/generics-01-starting-setup/src/app.ts
@@ -10,3 +10,10 @@
 // promise.then(data => {
 //     // data.split(' ');
 // })
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergeObj.age);
