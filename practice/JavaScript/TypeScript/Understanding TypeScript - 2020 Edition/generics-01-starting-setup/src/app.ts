5dcc14e8400259f27d0046bcfc712acd30cf0636
dobernike <Dobernike@mail.ru>
2020-05-08 07:37:35 +0300

    Working with Constraints

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/generics-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/generics-01-starting-setup/src/app.ts
@@ -11,9 +11,9 @@
 //     // data.split(' ');
 // })
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
 const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergeObj.age);
+console.log(mergeObj);
