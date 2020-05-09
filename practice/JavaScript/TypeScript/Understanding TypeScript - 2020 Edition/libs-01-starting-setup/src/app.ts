b04b999a63aa6bbe9c8a939717455de7e654ac10
dobernike <Dobernike@mail.ru>
2020-05-09 14:19:06 +0300

    TypeScript-embracing class-validator

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/libs-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/libs-01-starting-setup/src/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
 
 import { Product } from './product.model';
 
@@ -7,6 +8,16 @@ const products = [
   { title: 'A Carpet', price: 29.99 },
   { title: 'A Book', price: 10.99 },
 ];
+
+const newProd = new Product('', -5.99);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log('VALIDATION ERRORS!');
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
 
 // const p1 = new Product('A Book', 12.99);
 // console.log(prod.getInformation());
--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/libs-01-starting-setup/src/product.model.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/libs-01-starting-setup/src/product.model.ts
@@ -1,5 +1,10 @@
+import { IsNotEmpty, IsNumber, IsPositive } from 'class-validator';
+
 export class Product {
+  @IsNotEmpty()
   title: string;
+  @IsNumber()
+  @IsPositive()
   price: number;
 
   constructor(t: string, p: number) {
