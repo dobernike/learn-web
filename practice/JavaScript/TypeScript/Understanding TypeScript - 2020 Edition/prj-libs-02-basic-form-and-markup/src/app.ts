0d4decaf8353eb238d7f453a286f580d358ebde6
dobernike <Dobernike@mail.ru>
2020-05-09 15:29:25 +0300

    Getting User Input

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/prj-libs-02-basic-form-and-markup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/prj-libs-02-basic-form-and-markup/src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // send this to Google`s API!
+}
+
+form.addEventListener('submit', searchAddressHandler);
