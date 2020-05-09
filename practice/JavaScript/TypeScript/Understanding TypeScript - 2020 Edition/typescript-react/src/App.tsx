c5368899502f25bd59bfc2f5818d4b838a014890
dobernike <Dobernike@mail.ru>
2020-05-09 16:32:16 +0300

    Cross-Component Communication

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/typescript-react/src/App.tsx
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/typescript-react/src/App.tsx
@@ -6,9 +6,13 @@ import NewTodo from './components/NewTodo';
 const App: React.FC = () => {
   const todos = [{ id: 't1', text: 'Finish the course' }];
 
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/typescript-react/src/components/NewTodo.tsx
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/typescript-react/src/components/NewTodo.tsx
@@ -1,12 +1,16 @@
 import React, { useRef } from 'react';
 
-const NewTodo: React.FC = () => {
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
   const textInputRef = useRef<HTMLInputElement>(null);
 
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     const enteredText = textInputRef.current!.value;
-    console.log(enteredText);
+    props.onAddTodo(enteredText);
   };
 
   return (
