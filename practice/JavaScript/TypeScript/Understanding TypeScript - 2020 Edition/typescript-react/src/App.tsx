a2661d1674c964fa8daead56e33873d4067eb6bc
dobernike <Dobernike@mail.ru>
2020-05-09 16:37:55 +0300

    Working with State & Types

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/typescript-react/src/App.tsx
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/typescript-react/src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
 
 const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'Finish the course' }];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos([{ id: Math.random().toString(), text: text }]);
   };
 
   return (
