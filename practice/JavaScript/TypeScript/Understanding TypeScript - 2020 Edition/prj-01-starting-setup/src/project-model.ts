bb8b171515bc0f2f224450e76c25213b21034be8
dobernike <Dobernike@mail.ru>
2020-05-09 10:37:51 +0300

    Working with Namespaces

---
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/prj-01-starting-setup/src/project-model.ts
@@ -0,0 +1,17 @@
+// Project Type
+namespace App {
+  export enum ProjectStatus {
+    Active,
+    Finished,
+  }
+
+  export class Project {
+    constructor(
+      public id: string,
+      public title: string,
+      public description: string,
+      public people: number,
+      public status: ProjectStatus
+    ) {}
+  }
+}
