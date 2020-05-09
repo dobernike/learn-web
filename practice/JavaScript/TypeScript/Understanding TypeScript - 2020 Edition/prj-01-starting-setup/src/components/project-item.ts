4aeaf692a9a0802a8cebf0a220bf94fa7401c6d8
dobernike <Dobernike@mail.ru>
2020-05-09 10:49:59 +0300

    Organizing Files & Folders

---
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/prj-01-starting-setup/src/components/project-item.ts
@@ -0,0 +1,47 @@
+/// <reference path="base-component.ts" />
+
+// ProjectItem Class
+namespace App {
+  export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
+    implements Draggable {
+    private project: Project;
+
+    get persons() {
+      if (this.project.people === 1) {
+        return '1 person';
+      } else {
+        return `${this.project.people} persons`;
+      }
+    }
+
+    constructor(hostId: string, project: Project) {
+      super('single-project', hostId, false, project.id);
+      this.project = project;
+
+      this.configure();
+      this.renderContent();
+    }
+
+    @autobind
+    dragStartHandler(event: DragEvent) {
+      event.dataTransfer!.setData('text/plain', this.project.id);
+      event.dataTransfer!.effectAllowed = 'move';
+    }
+
+    dragEndHandler(_: DragEvent) {
+      console.log('DragEnd');
+    }
+
+    configure() {
+      this.element.addEventListener('dragstart', this.dragStartHandler);
+      this.element.addEventListener('dragend', this.dragEndHandler);
+    }
+
+    renderContent() {
+      this.element.querySelector('h2')!.textContent = this.project.title;
+      this.element.querySelector('h3')!.textContent =
+        this.persons + ' assigned';
+      this.element.querySelector('p')!.textContent = this.project.description;
+    }
+  }
+}
---
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/prj-01-starting-setup/src/decorators/autobind.ts
@@ -0,0 +1,14 @@
+// autobind decorator
+namespace App {
+  export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjDescriptor;
+  }
+}
