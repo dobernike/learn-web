6ff3938554bbf72e6ceb850faf29f4abf4a0efb7
dobernike <Dobernike@mail.ru>
2020-05-09 16:45:45 +0300

    More Props & State Work

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/typescript-react/src/App.tsx
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/typescript-react/src/App.tsx
@@ -14,10 +14,16 @@ const App: React.FC = () => {
     ]);
   };
 
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 };
--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/typescript-react/src/components/TodoList.tsx
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/typescript-react/src/components/TodoList.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 
 interface TodoListProps {
   items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = (props) => {
   return (
     <ul>
       {props.items.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            DELETE
+          </button>
+        </li>
       ))}
     </ul>
   );
