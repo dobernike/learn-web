e3469df2411a368d87ea763841a5eabd2ee09ae4
dobernike <Dobernike@mail.ru>
2020-05-09 16:39:25 +0300

    Managing State Better

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/typescript-react/src/App.tsx
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/typescript-react/src/App.tsx
@@ -8,7 +8,10 @@ const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    setTodos([{ id: Math.random().toString(), text: text }]);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
   };
 
   return (
