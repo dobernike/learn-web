31cbce96f9530ec0b637d9cca8bcb6c208f346b1
dobernike <Dobernike@mail.ru>
2020-05-09 14:12:40 +0300

    No Types Needed class-transformer

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/libs-01-starting-setup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/libs-01-starting-setup/src/app.ts
@@ -1,6 +1,28 @@
-import _ from 'lodash';
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
 
-// declare var GLOBAL: string;
-// console.log(GLOBAL);
+import { Product } from './product.model';
 
-console.log(_.shuffle([1, 2, 3]));
+const products = [
+  { title: 'A Carpet', price: 29.99 },
+  { title: 'A Book', price: 10.99 },
+];
+
+// const p1 = new Product('A Book', 12.99);
+// console.log(prod.getInformation());
+
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
+
+// import _ from 'lodash'; // @types/lodash
+// console.log(_.shuffle([1, 2, 3]));
+
+// // declare var GLOBAL: string;
+// // console.log(GLOBAL);
---
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/libs-01-starting-setup/src/product.model.ts
@@ -0,0 +1,13 @@
+export class Product {
+  title: string;
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+
+  getInformation() {
+    return [this.title, `${this.price}`];
+  }
+}
