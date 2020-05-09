52156acfab0d3029ed1d2bfc73195ef689797832
dobernike <Dobernike@mail.ru>
2020-05-09 15:51:31 +0300

    fix error

--- practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/prj-libs-02-basic-form-and-markup/src/app.ts
+++ practice/JavaScript/TypeScript/Understanding TypeScript - 2020 Edition/prj-libs-02-basic-form-and-markup/src/app.ts
@@ -27,7 +27,7 @@ function searchAddressHandler(event: Event) {
         throw new Error('Could not fetch location!');
       }
       const coordinates = response.data.results[0].geometry.location;
-      const map = new google.maps.Map(document.getElementById('map'), {
+      const map = new google.maps.Map(document.getElementById('map')!, {
         center: coordinates,
         zoom: 16,
       });
