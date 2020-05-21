24ca66e81ca42f245f944fa1a181263ba97cb16b
dobernike <dobernike@mail.ru>
2020-05-21 09:14:45 +0300

    9: globalShortcuts and Accelerators

--- practice/Electron/codedamn_course/hello-world/index.js
+++ practice/Electron/codedamn_course/hello-world/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
 // const url = require('url');
 
 let win = null;
@@ -35,6 +35,18 @@ function boot() {
   //   win2.on('closed', (_) => {
   //     console.log('win2 was closed');
   //   });
+
+  globalShortcut.register('CommandOrControl+1', () => {
+    win.isMaximized() ? win.unmaximize() : win.maximize();
+  });
+
+  // globalShortcut.register('Super+F', () => {
+  //   alert('Super key pressed');
+  // });
+
+  // globalShortcut.register('Alt+1', () => {
+  //   alert('alt key pressed');
+  // });
 }
 
 app.on('ready', boot);
