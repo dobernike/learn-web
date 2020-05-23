5e692a03bc25294a97b653fd341b7112cc898d70
dobernike <dobernike@mail.ru>
2020-05-23 23:30:48 +0300

    stopping

--- practice/Electron/redit_clone/electron_react_example/src/App.js
+++ practice/Electron/redit_clone/electron_react_example/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    axios
+      .get('https://reddit.com/r/aww.json')
+      .then((response) => {
+        const data = response.data.data.children;
+        console.log(data);
+      })
+      .catch(console.log);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Cras justo odio</li>
+          <li className="list-group-item">Dapibus ac facilisis in</li>
+          <li className="list-group-item">Morbi leo risus</li>
+          <li className="list-group-item">Porta ac consectetur ac</li>
+          <li className="list-group-item">Vestibulum at eros</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
