393448031e4d935457a9d54cb268b98e73b18933
dobernike <dobernike@mail.ru>
2020-11-11 19:56:59 +0300

    init resource list

--- practice/JavaScript/React/framework-compare/src/components/ResourceList.js
+++ practice/JavaScript/React/framework-compare/src/components/ResourceList.js
@@ -1,48 +1,46 @@
+import { useState } from 'react';
+
+const initialResources = [
+  {
+    _id: '1',
+    title: 'resource 1',
+    description: 'Description 1',
+    link: 'https://google.com',
+    type: 'blog',
+  },
+  {
+    _id: '2',
+    title: 'resource 2',
+    description: 'Description 2',
+    link: 'https://google.com',
+    type: 'video',
+  },
+  {
+    _id: '3',
+    title: 'resource 3',
+    description: 'Description 3',
+    link: 'https://google.com',
+    type: 'book',
+  },
+];
+
 const ResourceList = () => {
+  const [resources, setResources] = useState(initialResources);
+
   return (
     <ul className="list-group mb-3 resource-list">
-      <li className="list-group-item d-flex justify-content-between lh-condensed">
-        <div>
-          <h6 className="my-0">Resource 1</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">type</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between lh-condensed">
-        <div>
-          <h6 className="my-0">Resource 2</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">type</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between lh-condensed">
-        <div>
-          <h6 className="my-0">Resource 3</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">type</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between">
-        <div>
-          <h6 className="my-0">Resource 4</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">type</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between">
-        <div>
-          <h6 className="my-0">Resource 5</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">type</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between bg-light">
-        <div className="text-success">
-          <h6 className="my-0">Resource 6</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">type</span>
-      </li>
+      {resources.map((resources) => (
+        <li
+          key={resources._id}
+          className="list-group-item d-flex justify-content-between lh-condensed"
+        >
+          <div>
+            <h6 className="my-0">{resources.title}</h6>
+            <small className="text-muted">{resources.description}</small>
+          </div>
+          <span className="text-muted">{resources.type}</span>
+        </li>
+      ))}
     </ul>
   );
 };
