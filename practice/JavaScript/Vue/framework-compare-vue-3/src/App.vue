25dd8cbdb8d6bc26a7a124d9c7ed83272b0a6957
dobernike <dobernike@mail.ru>
2020-11-15 11:14:33 +0300

    Counter

--- practice/JavaScript/Vue/framework-compare-vue-3/src/App.vue
+++ practice/JavaScript/Vue/framework-compare-vue-3/src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="my-app">
-    <h1>Hello World</h1>
+    <h1>{{ counter }}</h1>
+    <button v-on:click="increment(1)">Increment</button>
+    <button v-on:click="increment(-1)">Decrement</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      counter: 100,
+    };
+  },
+  methods: {
+    increment(inc) {
+      this.counter += inc;
+    },
+  },
+};
 </script>
 
 <style>
 .my-app {
   color: blue;
+  padding: 15rem;
+}
+
+h1 {
+  font-size: 50px;
 }
 </style>
