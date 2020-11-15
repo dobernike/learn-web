267b39619039c5b229acd06a2638587c9a7781d7
dobernike <dobernike@mail.ru>
2020-11-15 13:11:34 +0300

    V-for iteration

--- practice/JavaScript/Vue/framework-compare-vue-3/src/components/ResourceList.vue
+++ practice/JavaScript/Vue/framework-compare-vue-3/src/components/ResourceList.vue
@@ -1,49 +1,17 @@
 <template>
   <ul class="list-group mb-3">
-    <li class="list-group-item d-flex justify-content-between lh-condensed">
+    <li
+      v-for="resource in resources"
+      :key="resource._id"
+      class="list-group-item d-flex justify-content-between lh-condensed"
+    >
       <div>
-        <h6 class="my-0">Resource 1</h6>
-        <small class="text-muted">Brief description</small>
+        <h6 class="my-0">{{ resource.title }}</h6>
+        <small class="text-muted">{{ resource.description }}</small>
       </div>
-      <span class="text-muted">type</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between lh-condensed">
-      <div>
-        <h6 class="my-0">Resource 2</h6>
-        <small class="text-muted">Brief description</small>
-      </div>
-      <span class="text-muted">type</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between lh-condensed">
-      <div>
-        <h6 class="my-0">Resource 3</h6>
-        <small class="text-muted">Brief description</small>
-      </div>
-      <span class="text-muted">type</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between">
-      <div>
-        <h6 class="my-0">Resource 4</h6>
-        <small class="text-muted">Brief description</small>
-      </div>
-      <span class="text-muted">type</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between">
-      <div>
-        <h6 class="my-0">Resource 5</h6>
-        <small class="text-muted">Brief description</small>
-      </div>
-      <span class="text-muted">type</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between bg-light">
-      <div class="text-success">
-        <h6 class="my-0">Resource 6</h6>
-        <small class="text-muted">Brief description</small>
-      </div>
-      <span class="text-muted">type</span>
+      <span class="text-muted">{{ resource.type }}</span>
     </li>
   </ul>
-  {{ resources }}
 </template>
 
 <script>
