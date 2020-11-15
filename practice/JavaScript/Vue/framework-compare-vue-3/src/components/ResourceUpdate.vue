a4ba1f69741f038d630c2ae2b31005d38180861b
dobernike <dobernike@mail.ru>
2020-11-15 23:51:42 +0300

    mixins

--- practice/JavaScript/Vue/framework-compare-vue-3/src/components/ResourceUpdate.vue
+++ practice/JavaScript/Vue/framework-compare-vue-3/src/components/ResourceUpdate.vue
@@ -57,6 +57,7 @@
 
 <script>
 import { updateResource } from '@/actions';
+import alertMixin from '@/mixins/alert';
 export default {
   props: {
     resource: Object,
@@ -65,10 +66,9 @@ export default {
     return {
       uResource: { ...this.resource },
       types: ['blog', 'video', 'book'],
-      alert: this.initAlert(),
-      timeoutId: null,
     };
   },
+  mixins: [alertMixin],
   emits: ['on-resource-update'],
   beforeUnmount() {
     this.clearAlertTimeout();
@@ -84,17 +84,6 @@ export default {
     },
   },
   methods: {
-    initAlert() {
-      return { success: null, error: null };
-    },
-    clearAlertTimeout() {
-      this.timeoutId && clearTimeout(this.timeoutId);
-    },
-    setAlert(type, message) {
-      this.alert = this.initAlert();
-      this.alert[type] = message;
-      this.timeoutId = setTimeout(() => (this.alert = this.initAlert()), 2000);
-    },
     async submitForm() {
       try {
         const updatedResource = await updateResource(
