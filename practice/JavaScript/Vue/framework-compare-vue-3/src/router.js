acdb945ff9594d0fa5c7de2481646a00bb530788
dobernike <dobernike@mail.ru>
2020-11-16 22:31:37 +0300

    children routes

--- practice/JavaScript/Vue/framework-compare-vue-3/src/router.js
+++ practice/JavaScript/Vue/framework-compare-vue-3/src/router.js
@@ -1,11 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+// import ResourceRoutes from '@/pages/ResourceRoutes';
 import ResourceHome from '@/pages/ResourceHome';
 import ResourceNew from '@/pages/ResourceNew';
 
+// const routes = [
+//   { path: '/', redirect: { name: 'resourceHomePage' } },
+//   {
+//     path: '/resources',
+//     name: 'resourcesRoutes',
+//     component: ResourceRoutes,
+//     children: [
+//       {
+//         path: '',
+//         name: 'resourceHomePage',
+//         component: ResourceHome,
+//       },
+//       {
+//         path: 'new',
+//         name: 'resourceNewPage',
+//         component: ResourceNew,
+//       },
+//     ],
+//   },
+// ];
+
 const routes = [
-  { path: '/', component: ResourceHome },
-  { path: '/new', component: ResourceNew },
+  { path: '/', redirect: { name: 'resourceHomePage' } },
+  { path: '/resources', name: 'resourceHomePage', component: ResourceHome },
+  { path: '/resources/new', name: 'resourceNewPage', component: ResourceNew },
 ];
 
 const router = createRouter({
