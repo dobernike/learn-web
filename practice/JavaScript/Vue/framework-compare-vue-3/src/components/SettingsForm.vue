51546b87970ce647783837ee53609a80badb5b25
dobernike <dobernike@mail.ru>
2020-12-02 22:56:37 +0300

    Settings form

---
+++ practice/JavaScript/Vue/framework-compare-vue-3/src/components/SettingsForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <form>
+    <div class="mb-3">
+      <label htmlFor="fontSize">Font Size</label>
+      <input
+        :value="fontSize"
+        @input="handleFontSize($event.target.value)"
+        type="text"
+        class="form-control"
+        id="fontSize"
+        placeholder="17"
+      />
+    </div>
+    <div class="mb-3">
+      <label htmlFor="theme">Theme</label>
+      <select
+        :value="theme"
+        @input="handleTheme($event.target.value)"
+        id="theme"
+        class="form-control"
+      >
+        <option value="dark">Dark</option>
+        <option value="light">Light</option>
+      </select>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    theme: {
+      type: String,
+    },
+    fontSize: {
+      type: String,
+    },
+  },
+  emits: ['update:theme', 'update:fontSize'],
+  methods: {
+    handleTheme(theme) {
+      this.$emit('update:theme', theme);
+    },
+    handleFontSize(fontSize) {
+      this.$emit('update:fontSize', fontSize);
+    },
+  },
+};
+</script>
--- practice/JavaScript/Vue/framework-compare-vue-3/src/components/SettingsModal.vue
+++ practice/JavaScript/Vue/framework-compare-vue-3/src/components/SettingsModal.vue
@@ -6,22 +6,36 @@
     @on-submit="submitSettings"
   >
     <div>
-      My custom content
+      <settings-form v-model:fontSize="fontSize" v-model:theme="theme" />
+      <!-- <settings-form
+        :fontSize="fontSize"
+        :theme="theme"
+        @update:fontSize="fontSize = $event"
+        @update:theme="theme = $event"
+      /> -->
     </div>
   </modal>
 </template>
 
 <script>
 import Modal from '@/components/shared/Modal';
+import SettingsForm from '@/components/SettingsForm';
 export default {
-  components: { Modal },
+  components: { Modal, SettingsForm },
   data() {
     return {
       isOpen: false,
+      fontSize: '',
+      theme: '',
     };
   },
   methods: {
     submitSettings() {
+      const settings = {
+        fontSize: this.fontSize,
+        theme: this.theme,
+      };
+      alert(JSON.stringify(settings));
       this.isOpen = false;
     },
   },
