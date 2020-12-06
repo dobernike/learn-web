c6bc6e82e3d6b5054efecc7fd11aa120de957188
dobernike <dobernike@mail.ru>
2020-12-06 22:41:10 +0300

    setup js

---
+++ practice/JavaScript/Vue/prj-monster/app.js
@@ -0,0 +1,25 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100
+    };
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+  },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    }
+  }
+});
+
+app.mount('#game')
