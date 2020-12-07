0f289f804bf3d25f8fd01c6aa017540075cc21a8
dobernike <dobernike@mail.ru>
2020-12-07 21:04:04 +0300

    add special attack

--- practice/JavaScript/Vue/prj-monster/app.js
+++ practice/JavaScript/Vue/prj-monster/app.js
@@ -2,11 +2,18 @@ function getRandomValue(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function getHealth(health, attack) {
+  return health > attack
+    ? health - attack
+    : 0;
+}
+
 const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      monsterHealth: 100
+      monsterHealth: 100,
+      manaForSpecialAttack: 3,
     };
   },
   computed: {
@@ -15,23 +22,29 @@ const app = Vue.createApp({
     },
     playerBarStyles() {
       return { width: this.playerHealth + '%' };
+    },
+    mayUseSpecialAttack() {
+      return this.manaForSpecialAttack < 3;
     }
   },
   methods: {
     attackMonster() {
-      const attackValue = getRandomValue(5, 12);
-      this.monsterHealth = this.monsterHealth > attackValue
-        ? this.monsterHealth - attackValue
-        : 0;
-      this.attackPlayer();
+      this.manaForSpecialAttack++
+      const attackValue = getRandomValue(5, 12)
+      this.monsterHealth = getHealth(this.monsterHealth, attackValue)
+      this.attackPlayer()
   },
     attackPlayer() {
-      const attackValue = getRandomValue(8, 15);
-      this.playerHealth = this.playerHealth > attackValue
-        ? this.playerHealth - attackValue
-        : 0;
+      const attackValue = getRandomValue(8, 15)
+      this.playerHealth = getHealth(this.playerHealth, attackValue)
+    },
+    specialAttackMonster() {
+      this.manaForSpecialAttack = 0
+      const attackValue = getRandomValue(10, 25)
+      this.monsterHealth = getHealth(this.monsterHealth, attackValue)
+      this.attackPlayer()
     }
   }
-});
+})
 
 app.mount('#game')
