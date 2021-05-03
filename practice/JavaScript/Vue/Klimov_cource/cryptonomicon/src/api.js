e0e5c0a47fdfc33525043d8aec44ea589c3f662a
dobernike <dobernike@mail.ru>
2021-05-03 22:45:38 +0300

    changed LS to BroadcastChannel

--- practice/JavaScript/Vue/Klimov_cource/cryptonomicon/src/api.js
+++ practice/JavaScript/Vue/Klimov_cource/cryptonomicon/src/api.js
@@ -30,7 +30,7 @@ const connect = () => {
 
     if (type !== AGGREGATE_INDEX || !price || !ticker) return;
 
-    message_broadcast(message);
+    messageBroadcast(message);
     updateTickerPrice(ticker, price);
   });
 };
@@ -44,11 +44,27 @@ function updateTickerPrice(ticker, price) {
 const CRYPTONOMICON_MESSAGE = 'cryptonomicon-message';
 const CRYPTONOMICON_MASTER_TAB_ACTIVE = 'cryptonomicon-master-tab-active';
 
-const setMasterTabActive = () =>
-  localStorage.setItem(CRYPTONOMICON_MASTER_TAB_ACTIVE, true);
+// doesn't work in Safari, use localStorage instead
+const broadcastChannel = new BroadcastChannel('broadcast-channel');
+broadcastChannel.onmessage = (event) => {
+  console.log(event);
+  if (event.data[CRYPTONOMICON_MESSAGE]) {
+    const message = JSON.parse(event.data[CRYPTONOMICON_MESSAGE]);
+    const { PRICE: price, FROMSYMBOL: ticker } = message;
+    updateTickerPrice(ticker, price);
+  }
 
-const setMasterTabInactive = () =>
-  localStorage.setItem(CRYPTONOMICON_MASTER_TAB_ACTIVE, false);
+  if (event.data[CRYPTONOMICON_MASTER_TAB_ACTIVE]) {
+    const isMasterTabActive = JSON.parse(
+      event.data[CRYPTONOMICON_MASTER_TAB_ACTIVE]
+    );
+    if (isMasterTabActive) return;
+    connect();
+  }
+};
+
+const setMasterTabActive = () => masterTabActiveBroadcast(true);
+const setMasterTabInactive = () => masterTabActiveBroadcast(false);
 
 const sendToWebSocket = (message) => {
   const stringifiedMessage = JSON.stringify(message);
@@ -93,24 +109,16 @@ export const unsubscribeFromTicker = (ticker) => {
   unsubscribeFromTickerOnWs(ticker);
 };
 
-window.addEventListener('storage', message_receive);
-
-function message_broadcast(message) {
-  localStorage.setItem(CRYPTONOMICON_MESSAGE, JSON.stringify(message));
+function messageBroadcast(message) {
+  broadcastChannel.postMessage({
+    [CRYPTONOMICON_MESSAGE]: JSON.stringify(message),
+  });
 }
 
-function message_receive(event) {
-  if (event.key == CRYPTONOMICON_MESSAGE) {
-    const message = JSON.parse(event.newValue);
-    const { PRICE: price, FROMSYMBOL: ticker } = message;
-    updateTickerPrice(ticker, price);
-  }
-
-  if (event.key === CRYPTONOMICON_MASTER_TAB_ACTIVE) {
-    const isMasterTabActive = JSON.parse(event.newValue);
-    if (isMasterTabActive) return;
-    connect();
-  }
+function masterTabActiveBroadcast(message) {
+  broadcastChannel.postMessage({
+    [CRYPTONOMICON_MASTER_TAB_ACTIVE]: JSON.stringify(message),
+  });
 }
 
 window.addEventListener('beforeunload', setMasterTabInactive);
