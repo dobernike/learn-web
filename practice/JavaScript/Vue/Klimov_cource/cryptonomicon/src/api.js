167ae237810ff325c39d7f940e9758fae5aa2a74
dobernike <dobernike@mail.ru>
2021-05-03 22:06:41 +0300

    hw added shared with LS

--- practice/JavaScript/Vue/Klimov_cource/cryptonomicon/src/api.js
+++ practice/JavaScript/Vue/Klimov_cource/cryptonomicon/src/api.js
@@ -15,32 +15,54 @@ const MARKET = 'CCCAGG';
 
 const tickersHandlers = new Map();
 
-const socket = new WebSocket(
-  `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
-);
+let socket;
+const connect = () => {
+  socket = new WebSocket(
+    `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
+  );
+  [...tickersHandlers.keys()].forEach((ticker) =>
+    subscribeToTickerOnWs(ticker)
+  );
 
-socket.addEventListener('message', (event) => {
-  const data = JSON.parse(event.data);
-  const { TYPE: type, PRICE: price, FROMSYMBOL: ticker } = data;
+  socket.addEventListener('message', (event) => {
+    const message = JSON.parse(event.data);
+    const { TYPE: type, PRICE: price, FROMSYMBOL: ticker } = message;
 
-  if (type !== AGGREGATE_INDEX || !price || !ticker) return;
+    if (type !== AGGREGATE_INDEX || !price || !ticker) return;
 
+    message_broadcast(message);
+    updateTickerPrice(ticker, price);
+  });
+};
+connect();
+
+function updateTickerPrice(ticker, price) {
   const handlers = tickersHandlers.get(ticker);
   handlers.forEach((cb) => cb(price));
-});
+}
+
+const CRYPTONOMICON_MESSAGE = 'cryptonomicon-message';
+const CRYPTONOMICON_MASTER_TAB_ACTIVE = 'cryptonomicon-master-tab-active';
+
+const setMasterTabActive = () =>
+  localStorage.setItem(CRYPTONOMICON_MASTER_TAB_ACTIVE, true);
+
+const setMasterTabInactive = () =>
+  localStorage.setItem(CRYPTONOMICON_MASTER_TAB_ACTIVE, false);
 
 const sendToWebSocket = (message) => {
   const stringifiedMessage = JSON.stringify(message);
-
-  if (socket.readyState === WebSocket.OPEN) {
+  if (socket?.readyState === WebSocket.OPEN) {
     socket.send(stringifiedMessage);
+    setMasterTabActive();
     return;
   }
 
-  socket.addEventListener(
+  socket?.addEventListener(
     'open',
     () => {
       socket.send(stringifiedMessage);
+      setMasterTabActive();
     },
     { once: true }
   );
@@ -70,3 +92,25 @@ export const unsubscribeFromTicker = (ticker) => {
   tickersHandlers.delete(ticker);
   unsubscribeFromTickerOnWs(ticker);
 };
+
+window.addEventListener('storage', message_receive);
+
+function message_broadcast(message) {
+  localStorage.setItem(CRYPTONOMICON_MESSAGE, JSON.stringify(message));
+}
+
+function message_receive(event) {
+  if (event.key == CRYPTONOMICON_MESSAGE) {
+    const message = JSON.parse(event.newValue);
+    const { PRICE: price, FROMSYMBOL: ticker } = message;
+    updateTickerPrice(ticker, price);
+  }
+
+  if (event.key === CRYPTONOMICON_MASTER_TAB_ACTIVE) {
+    const isMasterTabActive = JSON.parse(event.newValue);
+    if (isMasterTabActive) return;
+    connect();
+  }
+}
+
+window.addEventListener('beforeunload', setMasterTabInactive);
