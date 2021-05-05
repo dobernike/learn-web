ed77d6549aace2461b76cf43d4d6edb51645feea
dobernike <dobernike@mail.ru>
2021-05-05 23:40:07 +0300

    added red color if we haven't price

--- practice/JavaScript/Vue/Klimov_cource/cryptonomicon/src/App.vue
+++ practice/JavaScript/Vue/Klimov_cource/cryptonomicon/src/App.vue
@@ -114,7 +114,11 @@
             }"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
           >
-            <div @click="selectTicker(t)" class="px-4 py-5 sm:p-6 text-center">
+            <div
+              @click="selectTicker(t)"
+              :class="{ 'bg-red-100': t.price === '-' }"
+              class="px-4 py-5 sm:p-6 text-center"
+            >
               <dt class="text-sm font-medium text-gray-500 truncate">
                 {{ t.name }} - USD
               </dt>
