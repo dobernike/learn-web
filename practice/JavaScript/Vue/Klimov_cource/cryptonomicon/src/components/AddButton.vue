372def2121a7e7d8eac9501e7adb03ffa6170b81
dobernike <dobernike@mail.ru>
2021-05-08 19:11:32 +0300

    added emit to click for more readable native evnts

--- practice/JavaScript/Vue/Klimov_cource/cryptonomicon/src/components/AddButton.vue
+++ practice/JavaScript/Vue/Klimov_cource/cryptonomicon/src/components/AddButton.vue
@@ -1,6 +1,7 @@
 <template>
   <button
     type="button"
+    @click="$emit('click')"
     :disabled="disabled"
     :class="{ 'opacity-50': disabled }"
     class="inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
@@ -30,6 +31,10 @@ export default {
       required: false,
       default: false,
     },
+  },
+
+  emits: {
+    click: null,
   },
 };
 </script>
