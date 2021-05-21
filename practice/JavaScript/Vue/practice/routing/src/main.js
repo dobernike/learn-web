2559087fd421909b2b694bc27edc8a6e548e0c4d
dobernike <dobernike@mail.ru>
2021-05-21 12:39:41 +0300

    routing setup

--- practice/JavaScript/Vue/practice/routing/src/main.js
+++ practice/JavaScript/Vue/practice/routing/src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 
-const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(),
+  routes: []
+});
+
+const app = createApp(App);
 
 app.mount('#app');
