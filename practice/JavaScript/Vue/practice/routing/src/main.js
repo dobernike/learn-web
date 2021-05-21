53a47f8efb632cee0d212302796aeca2e654e8cf
dobernike <dobernike@mail.ru>
2021-05-21 12:56:30 +0300

    registering & rendering routes

--- practice/JavaScript/Vue/practice/routing/src/main.js
+++ practice/JavaScript/Vue/practice/routing/src/main.js
@@ -2,12 +2,19 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: []
+  routes: [
+    { path: '/teams', component: TeamsList },
+    { path: '/users', component: UsersList }
+  ]
 });
 
 const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
