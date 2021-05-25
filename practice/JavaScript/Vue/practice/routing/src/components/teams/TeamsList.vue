7a3f09ef7e939059a1b31868b3385317598cdacc
dobernike <dobernike@mail.ru>
2021-05-25 21:50:25 +0300

    navigation & dynamic

--- practice/JavaScript/Vue/practice/routing/src/components/teams/TeamsList.vue
+++ practice/JavaScript/Vue/practice/routing/src/components/teams/TeamsList.vue
@@ -3,6 +3,7 @@
     <teams-item
       v-for="team in teams"
       :key="team.id"
+      :id="team.id"
       :name="team.name"
       :member-count="team.members.length"
     ></teams-item>
@@ -14,9 +15,9 @@ import TeamsItem from './TeamsItem.vue';
 
 export default {
   components: {
-    TeamsItem,
+    TeamsItem
   },
-  inject: ['teams'],
+  inject: ['teams']
 };
 </script>
 
