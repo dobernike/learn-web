f4ec5ddc7c9d07c1379f803bce47faef984e084f
dobernike <dobernike@mail.ru>
2021-05-27 22:15:10 +0300

    wip

--- practice/JavaScript/Vue/practice/routing/src/main.js
+++ practice/JavaScript/Vue/practice/routing/src/main.js
@@ -9,7 +9,8 @@ import TeamMembers from './components/teams/TeamMembers.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/teams', component: TeamsList },
+    // { path: '/', redirect: '/teams' },
+    { path: '/teams', component: TeamsList, alias: '/' },
     { path: '/users', component: UsersList },
     { path: '/teams/:teamId', component: TeamMembers, props: true }
   ],
